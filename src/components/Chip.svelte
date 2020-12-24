b700f6bb078b5e804837512768fe67c31d818c95
rotimi_best <best@classroomio.com>
2020-12-24 20:32:05 +0200

    Home page

---
+++ src/components/Chip.svelte
@@ -0,0 +1,21 @@
+<script>
+	export let value;
+</script>
+
+<p>{value}</p>
+
+<style>
+  p {
+    display: inline-block;
+    min-width: 20px;
+    padding: 0 6px;
+    font-size: 12px;
+    font-weight: 500;
+    line-height: 18px;
+    color: #586069;
+    text-align: center;
+    background-color: #d1d5da80;
+    border: 1px solid transparent;
+    border-radius: 2em;
+  }
+</style>
