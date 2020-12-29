fe3cdd10615a8b8e269251ad88829e481fe05c89
rotimi_best <rotimiibitoyeemma@gmail.com>
2020-12-29 19:05:18 +0200

    Footer component

---
+++ src/components/Footer.svelte
@@ -0,0 +1,15 @@
+<script>
+
+</script>
+
+<style>
+  /* your styles go here */
+</style>
+
+<!-- markup (zero or more items) goes here -->
+<div class="mx-auto my-1 lg:w-4/5 sm:w-full">
+  <hr />
+  <div class="flex justify-center w-full">
+    <span class="text-gray-700 text-sm py-2">Created with love by <a class="underline text-blue-700" href="https://twitter.com/rotimi_best" target="_blank">Rotimi Best</a></span>
+  </div>
+</div>
