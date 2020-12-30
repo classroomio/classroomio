63f404013bbdf47605a7b6d687bd9fb8d5fc7d91
rotimi_best <best@classroomio.com>
2020-12-30 22:29:01 +0200

    Ask page

--- src/routes/ask.svelte
+++ src/routes/ask.svelte
@@ -1,7 +1,50 @@
+<script context="module">
+  import ChevronDown24 from "carbon-icons-svelte/lib/ChevronDown24";
+	import TextEditor from "../components/TextEditor.svelte";
+
+  let questionTitle = '';
+  let questionTags = [];
+  let questionDescription = '';
+
+  const faculties = [
+    'ИЭКСУ', 'ИМБТ', 'ИМИ', 'ИПТДМ', 'ИИИР', 'ГФ', 'ХТФ', 'ИДЗО', 'УИИ', 'УНИ'
+  ];
+</script>
+
 <svelte:head>
-	<title>About</title>
+	<title>Спросить собшество вопрос</title>
 </svelte:head>
 
-<h1>About this site</h1>
+<div class="lg:w-3/5 mx-auto md:mx-10 lg:mb-20">
+  <h2 class="pb-2">Спросить собшество вопрос</h2>
+  <div class="my-1 px-1">
+    <button class="flex border rounded-lg border-grey">
+      <span>Факултеты</span> <ChevronDown24 class="ml-2" />
+    </button>
+    <div>
+      {#each faculties as faculty}
+        <div>
+          <input type="checkbox" class="appearance-none checked:bg-blue-600 checked:border-transparent">
+          <span>{faculty}</span>
+        </div>
+      {/each}
+    </div>
+    <input
+      class="title rounded-lg border border-1 border-gray p-1 w-full"
+      error="false"
+      autocomplete="off"
+      placeholder="Title"
+      aria-label="Title"
+      aria-describedby="title-input-validation"
+      type="text"
+      name="discussion[title]"
+      id="discussion_title"
+      value="{questionTitle}"
+    >
+  </div>
 
-<p>This is the 'about' page. There's not much here.</p>
+  <TextEditor
+    value="{questionDescription}"
+    placeholder="Задайте вопрос или начните разговор"
+  />
+</div>
