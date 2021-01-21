6869bcf79c8bbdefe71a8fa030fe381eb8de1ec0
rotimi_best <rotimiibitoyeemma@gmail.com>
2021-01-21 22:23:23 +0200

    Upvote update + complete ui of ask question

--- src/components/AskPage.svelte
+++ src/components/AskPage.svelte
@@ -1,49 +1,61 @@
-<script>
+<script context="module">
   import ChevronDown24 from "carbon-icons-svelte/lib/ChevronDown24";
-	import TextEditor from "../components/TextEditor.svelte";
+	import TextEditor from "./TextEditor.svelte";
 
   let questionTitle = '';
   let questionTags = [];
   let questionDescription = '';
-  let facultyFilterOpened = false;
+  let facultyFilterOpened = true;
 
   const faculties = [
     'ИЭКСУ', 'ИМБТ', 'ИМИ', 'ИПТДМ', 'ИИИР', 'ГФ', 'ХТФ', 'ИДЗО', 'УИИ', 'УНИ'
   ];
-  let checkbox = faculties.reduce((acc, cur) => {
-    acc[cur] = false;
-    return acc;
-  }, {});
 
   function handleFacultyFilters() {
+    console.log('clicking')
     facultyFilterOpened = facultyFilterOpened ? false : true;
   }
+  let things = [
+		{ id: 1, color: 'darkblue' },
+		{ id: 2, color: 'indigo' },
+		{ id: 3, color: 'deeppink' },
+		{ id: 4, color: 'salmon' },
+		{ id: 5, color: 'gold' }
+	];
+
+	function handleClick() {
+    things = things.slice(1);
+    console.log('things', things)
+	}
 </script>
 
 <svelte:head>
 	<title>Спросить собшество вопрос</title>
 </svelte:head>
 
+
 <div class="lg:w-3/5 mx-auto md:mx-10 lg:mb-20">
   <h2 class="pb-2">Спросить собшество вопрос</h2>
-  <div class="my-1 flex">
-    <div class="mr-1">
+  <div class="my-1 px-1">
+    <div class="mb-5">
+      <button on:click={handleClick}>
+        Remove first thing
+      </button>
+      {#each things as thing}
+        <p style="color: {thing.color}">{thing.id}</p>
+      {/each}
       <button
-        class="flex border rounded-lg border-grey py-2 px-3 focus:outline-none focus:border-gray-400 focus:bg-gray-200 relative"
+        class="flex border rounded-lg border-grey py-1 px-3 focus:outline-none focus:border-gray-400 focus:bg-gray-200"
         on:click={handleFacultyFilters}
       >
         <span>Факултеты</span> <ChevronDown24 class="ml-2" />
       </button>
 
       {#if facultyFilterOpened}
-        <div class="w-48 bg-white border border-grey rounded-lg mt-2 py-2 absolute">
+        <div class="w-48 bg-white border border-grey rounded-lg mt-2 py-2">
           {#each faculties as faculty}
             <div class="block px-4 py-2 border-b">
-              <input
-                type="checkbox"
-                class="border border-gray checked:bg-blue-600 checked:border-transparent mr-1 cursor-pointer"
-                bind:checked={checkbox[faculty]}
-              >
+              <input type="checkbox" class="border border-gray checked:bg-blue-600 checked:border-transparent mr-1 cursor-pointer">
               <span>{faculty}</span>
             </div>
           {/each}
