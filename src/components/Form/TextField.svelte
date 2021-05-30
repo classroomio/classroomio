f52d839dedb3e857f9365ce38616a4b277dfb636
Ibitoye Rotimi Best <best.rotimi@ikizmet.com>
2021-05-30 14:17:08 +0300

    Improve style of QandA

--- src/components/Form/TextField.svelte
+++ src/components/Form/TextField.svelte
@@ -8,9 +8,10 @@
 <label class="block">
   {#if label}<span class="text-gray-700">{label}</span>{/if}
   <input
-    class="form-input mt-1 block w-full rounded-lg"
+    class="form-input p-4 mt-1 block w-full rounded-t-lg bg-gray-100 border-t-0 border-l-0 border-r-0 border-b-1"
     type="text"
     on:keydown={onKeyDown}
     {placeholder}
-    bind:value />
+    bind:value
+  />
 </label>
