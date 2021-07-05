d985c6b3261171103d2fc195cfc334daebf8ebf9
Rotimi Best <best@classroomio.com>
2021-07-05 10:52:20 +0000

    Add Poll app

---
+++ src/components/Apps/components/Poll/components/Tabs.svelte
@@ -0,0 +1,19 @@
+<script>
+  export let tabs = [];
+
+  export let selectedTab;
+</script>
+
+<div class="mb-3">
+  {#each tabs as tab}
+    <button
+      class="mr-3 pb-2 border-t-0 border-l-0 border-r-0 {selectedTab ===
+      tab.value
+        ? 'border-b-4 border-indigo-600 focus:border-indigo-600'
+        : 'border-b-0'}"
+      on:click={() => (selectedTab = tab.value)}
+    >
+      {tab.label}
+    </button>
+  {/each}
+</div>
