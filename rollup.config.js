c35d79fa5f703f2490f2577c7404b43c5fa65e2c
Ibitoye Rotimi Best <best.rotimi@ikizmet.com>
2021-07-25 17:44:11 +0300

    Fetch exercise from server

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,21 @@
-import resolve from "@rollup/plugin-node-resolve";
-import replace from "@rollup/plugin-replace";
-import commonjs from "@rollup/plugin-commonjs";
-import svelte from "rollup-plugin-svelte";
-import babel from "@rollup/plugin-babel";
-import { terser } from "rollup-plugin-terser";
-import config from "sapper/config/rollup.js";
-import sveltePreprocess from "svelte-preprocess";
+import resolve from '@rollup/plugin-node-resolve';
+import replace from '@rollup/plugin-replace';
+import commonjs from '@rollup/plugin-commonjs';
+import svelte from 'rollup-plugin-svelte';
+import babel from '@rollup/plugin-babel';
+import { terser } from 'rollup-plugin-terser';
+import config from 'sapper/config/rollup.js';
+import sveltePreprocess from 'svelte-preprocess';
 
-import pkg from "./package.json";
+import pkg from './package.json';
 
 const mode = process.env.NODE_ENV;
-const dev = mode === "development";
+const dev = mode === 'development';
 const legacy = !!process.env.SAPPER_LEGACY_BUILD;
 
 const onwarn = (warning, onwarn) =>
-  (warning.code === "MISSING_EXPORT" && /'preload'/.test(warning.message)) ||
-  (warning.code === "CIRCULAR_DEPENDENCY" &&
+  (warning.code === 'MISSING_EXPORT' && /'preload'/.test(warning.message)) ||
+  (warning.code === 'CIRCULAR_DEPENDENCY' &&
     /[/\\]@sapper[/\\]/.test(warning.message)) ||
   onwarn(warning);
 
@@ -27,54 +27,54 @@ export default {
     output: config.client.output(),
     plugins: [
       replace({
-        "process.browser": true,
-        "process.env.NODE_ENV": JSON.stringify(mode),
-        preventAssignment: true
+        'process.browser': true,
+        'process.env.NODE_ENV': JSON.stringify(mode),
+        preventAssignment: true,
       }),
       svelte({
         preprocess,
         dev,
         hydratable: true,
-        emitCss: true
+        emitCss: true,
       }),
       resolve({
         browser: true,
-        dedupe: ["svelte"]
+        dedupe: ['svelte'],
       }),
       commonjs(),
 
       legacy &&
         babel({
-          extensions: [".js", ".mjs", ".html", ".svelte"],
-          babelHelpers: "runtime",
-          exclude: ["node_modules/@babel/**"],
+          extensions: ['.js', '.mjs', '.html', '.svelte'],
+          babelHelpers: 'runtime',
+          exclude: ['node_modules/@babel/**'],
           presets: [
             [
-              "@babel/preset-env",
+              '@babel/preset-env',
               {
-                targets: "> 0.25%, not dead"
-              }
-            ]
+                targets: '> 0.25%, not dead',
+              },
+            ],
           ],
           plugins: [
-            "@babel/plugin-syntax-dynamic-import",
+            '@babel/plugin-syntax-dynamic-import',
             [
-              "@babel/plugin-transform-runtime",
+              '@babel/plugin-transform-runtime',
               {
-                useESModules: true
-              }
-            ]
-          ]
+                useESModules: true,
+              },
+            ],
+          ],
         }),
 
       !dev &&
         terser({
-          module: true
-        })
+          module: true,
+        }),
     ],
 
     preserveEntrySignatures: false,
-    onwarn
+    onwarn,
   },
 
   server: {
@@ -82,27 +82,27 @@ export default {
     output: config.server.output(),
     plugins: [
       replace({
-        "process.browser": false,
-        "process.env.NODE_ENV": JSON.stringify(mode),
-        preventAssignment: true
+        'process.browser': false,
+        'process.env.NODE_ENV': JSON.stringify(mode),
+        preventAssignment: true,
       }),
       svelte({
         preprocess,
-        generate: "ssr",
+        generate: 'ssr',
         hydratable: true,
-        dev
+        dev,
       }),
       resolve({
-        dedupe: ["svelte"]
+        dedupe: ['svelte'],
       }),
-      commonjs()
+      commonjs(),
     ],
     external: Object.keys(pkg.dependencies).concat(
-      require("module").builtinModules
+      require('module').builtinModules
     ),
 
-    preserveEntrySignatures: "strict",
-    onwarn
+    preserveEntrySignatures: 'strict',
+    onwarn,
   },
 
   // serviceworker: {
