2e596cd6fb0f75e92a228db83a3172c7ab743914
Ibitoye Rotimi Best <best.rotimi@ikizmet.com>
2021-07-26 07:49:05 +0300

    Init work with making field required

--- src/components/Form/Select.svelte
+++ src/components/Form/Select.svelte
@@ -2,12 +2,17 @@
   export let label;
   export let value;
   export let options = [];
-  export let valueKey = "id";
+  export let valueKey = 'id';
+  export let isRequired = false;
 </script>
 
 <label class="block">
   {#if label} <span class="text-gray-700">{label}</span> {/if}
-  <select bind:value class="form-select block w-full mt-1">
+  <select
+    bind:value
+    class="form-select block w-full mt-1"
+    required={isRequired}
+  >
     {#each options as option}
       <option value={option[valueKey]}>
         {option.text}
