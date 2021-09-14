5fdd4010f461a116d35d166032c759da4082fcec
rotimi-best <rotimiibitoyeemma@gmail.com>
2021-09-14 07:01:44 +0300

    Make lesson draggable

--- src/components/PageBody/index.svelte
+++ src/components/PageBody/index.svelte
@@ -2,10 +2,11 @@
   // import
   //   border border-gray-300 rounded-md p-3 bg-blue-200 bg-opacity-25
   export let width = 'max-w-3xl w-11/12';
+  export let padding = 'pb-5 px-4';
   export let className = '';
 </script>
 
-<div class="root mt-4 mx-auto {width} {className} relative pb-5 px-4">
+<div class="root mt-4 mx-auto {width} {className} relative {padding}">
   {#if $$slots.header}
     <div
       class="head flex items-center justify-between sticky right-0 w-full py-3"
