84a4beb475a5b6b02b1bcabe33b1b84aaae25066
rotimi-best <best@classroomio.com>
2021-09-23 16:39:38 +0300

    v1 of deadline feature

--- src/components/Form/DateTime.svelte
+++ src/components/Form/DateTime.svelte
@@ -11,7 +11,7 @@
   <input
     bind:value
     {placeholder}
-    type="date"
+    type="datetime-local"
     class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
     on:blur={onChange}
   />
