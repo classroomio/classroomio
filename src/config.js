32ee665cb47b08b9b7e3da86379ed4befb723986
rotimi-best <best@classroomio.com>
2021-10-09 22:14:46 +0300

    CLASSROOMIO-T-24: Add sentry

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-  // isProd: process.env.NODE_ENV === 'development',
+  isProd: process.env.NODE_ENV !== 'development',
   supabaseConfig: {
     bucketPath:
       'https://koxqonvbkeakwvmdegcf.supabase.in/storage/v1/object/sign/',
