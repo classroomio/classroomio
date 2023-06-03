cfe85c1309a19b15834868c2882a0dc9ea99f869
Rotimi Best <rotimiibitoyeemma@gmail.com>
2023-06-03 08:07:32 +0100

    CLASSROOMIO-T-49: Begin work on quiz preview

---
+++ src/components/Org/Quiz/Play.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { fly } from 'svelte/transition';
+
+  const quiz = {};
+</script>
+
+<div
+  class="root absolute inset-0 flex justify-center items-center z-50 bg-white"
+  in:fly={{ y: 500, duration: 500 }}
+  out:fly={{ y: 500, duration: 500 }}
+>
+  <div>something</div>
+</div>
