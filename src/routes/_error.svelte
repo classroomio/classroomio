f368b17bf3e020b7885fd051bc7c656afa013876
tunny17 <tvnji17@gmail.com>
2023-07-13 20:27:55 +0100

    Unresolved Commits

--- src/routes/_error.svelte
+++ src/routes/_error.svelte
@@ -11,7 +11,6 @@
 
   export let status;
   import { appStore } from "../utils/store/app";
-  // icons
   import ErrorIcon from "../components/Icons/ErrorIcon.svelte";
   import ErrorIconDarkMode from "../components/Icons/ErrorIconDarkMode.svelte";
 </script>
@@ -23,9 +22,9 @@
 <div class="error-page flex flex-wrap items-center justify-center h-full py-10 dark:bg-gray-800">
 
   <div class="error-message w-2/4">
-    <h2>Our server is taking a nap😴.</h2>
-    <p class="dark:text-white">Don't worry, your learning is safe. Our server is just taking a quick break to recharge. In the meantime, feel free to export the rest of our website or take a nap yourself.</p>
-    <button on:click={() => {}}>Contact Us</button>
+    <h2 class="w-full text-3xl font-normal">Our server is taking a nap😴.</h2>
+    <p class="dark:text-white w-full text-base font-normal my-0.5">Don't worry, your learning is safe. Our server is just taking a quick break to recharge. In the meantime, feel free to export the rest of our website or take a nap yourself.</p>
+    <button on:click={() => window.open("mailto:support@classroomio.com", '_blank')}>Contact Us</button>
   </div>
 
   <div class="error-icon w-2/4 flex items-center justify-center">
@@ -42,17 +41,7 @@
 
 <style>
 
-  h2 {
-    width: 100%;
-    font-size: 2rem;
-    font-weight: 600;
-  }
-
   p {
-    width: 100%;
-    font-size: 16px;
-    font-weight: 400px;
-    line-height: 24px;
     color: #656565;
     margin: 5% 0;
   }
@@ -62,8 +51,6 @@
     background: #0233BD;
     border-radius: 10px;
     font-size: 14px;
-    font-weight: 400;
-    line-height: 18px;
     color: #fff;
   }
 
@@ -130,12 +117,33 @@
     }
   }
 
-  @media only screen
-    and (min-device-width: 375px)
-    and (max-device-width: 667px)
-    and (-webkit-min-device-pixel-ratio: 2) {
-  
+  /* iPhone 6, 6S, 7 and 8 */
+  @media only screen 
+    and (min-device-width: 375px) 
+    and (max-device-height: 667px)  
+    and (-webkit-device-pixel-ratio: 2) {
+    .error-page {
+      background: red;
+      padding: 10%;
+      flex-direction: column-reverse;
+    }
     
-  }  
+    .error-message {
+      width: 100%;
+    }  
+
+    h2 {
+      font-size: 1.5rem;
+    }
+
+    button {
+      padding: 4% 15%;
+    }
+
+    .error-icon {
+      width: 19rem;
+      height: 19rem;
+    }
+  }
 
 </style>
