3a3740435e85c28e12e65a753f05fd3346b8f00a
Rotimi Best <rotimiibitoyeemma@gmail.com>
2023-07-14 17:56:31 +0100

    CLASSROOMIO-T-48: Add sentry, redirect if 404

--- src/routes/_error.svelte
+++ src/routes/_error.svelte
@@ -1,30 +1,38 @@
 <script>
-  // export let error;
-  // const dev = process.env.NODE_ENV === 'development';
-  // let errorContent;
-  // function report() {}
-  // $: {
-  //   try {
-  //     errorContent = !!error ? JSON.stringify(error) : '';
-  //   } catch (error) {}
-  // }
-
-  export let status;
+  import { goto } from '@sapper/app';
   import { appStore } from '../utils/store/app';
   import ErrorIcon from '../components/Icons/ErrorIcon.svelte';
   import ErrorIconDarkMode from '../components/Icons/ErrorIconDarkMode.svelte';
+
+  export let status;
+  export let error;
+
+  if (status == 404) {
+    goto('/404');
+  }
+  console.error(error);
 </script>
 
 <svelte:head>
   <title>{status}</title>
 </svelte:head>
 
-<div class="flex lg:flex-row md:flex-col-reverse flex-wrap items-center justify-center h-full py-10 lg:px-6 dark:bg-gray-800">
-
+<div
+  class="flex lg:flex-row md:flex-col-reverse flex-wrap items-center justify-center h-full py-10 lg:px-6 dark:bg-gray-800"
+>
   <div class="md:w-full p-2 lg:w-2/4">
-    <h2 class="w-full text-lg lg:text-3xl font-medium lg:font-normal">Our server is taking a nap😴.</h2>
-    <p class="dark:text-white w-full text-base font-normal my-6">Don't worry, your learning is safe. Our server is just taking a quick break to recharge. In the meantime, feel free to export the rest of our website or take a nap yourself.</p>
-    <a class="hover:no-underline transition-all py-3 px-9 rounded-xl text-white mt-2" href="mailto:support@classroomio.com">Contact Us</a>
+    <h2 class="w-full text-lg lg:text-3xl font-medium lg:font-normal">
+      Something unexpected occured.
+    </h2>
+    <p class="dark:text-white w-full text-base font-normal my-6 text-gray-500">
+      Don't worry, your learning is safe. Our server is just taking a quick
+      break to recharge. In the meantime, feel free to export the rest of our
+      website or take a nap yourself.
+    </p>
+    <a
+      class="bg-blue-700 hover:bg-blue-900 hover:no-underline transition-all py-3 px-9 rounded-xl text-white mt-3"
+      href="mailto:support@classroomio.com">Contact Us</a
+    >
   </div>
 
   <div class="w-72 lg:w-2/4 flex items-center justify-center">
@@ -34,18 +42,5 @@
     {:else}
       <ErrorIcon />
     {/if}
-
   </div>
-
 </div>
-
-<style>
-  p {
-    color: #656565;
-  }
-
-  a {
-    background: #0233BD;
-  }
-
-</style>
