9b4df92d7867f4e23172a748dd983b389f781d53
tunny17 <tunji@digdippa.com>
2023-07-31 12:50:36 +0000

    CLASSROOMIO-T-62: Dark Mode Bug Fix
    CLASSROOMIO-T-62: Delete Unused class
    
    CLASSROOMIO-T-62: Dark Mode Bug Fix
    
    
    Co-authored-by: Tunji Adeyemi <tunji@digdippa.com>
    
    
    Merge-request: CLASSROOMIO-MR-23
    Merged-by: Best Admin <best@digdippa.com>

--- src/components/Course/components/Navigation/index.svelte
+++ src/components/Course/components/Navigation/index.svelte
@@ -230,6 +230,7 @@
       value="toggle"
       onClick={() => (show = !show)}
       size={$isMobile ? 'large' : 'small'}
+      color="text-black"
       toolTipProps={$isMobile
         ? {}
         : {
@@ -239,9 +240,9 @@
           }}
     >
       {#if show}
-        <ChevronLeft16 class="carbon-icon" />
+        <ChevronLeft16 />
       {:else}
-        <ChevronRight16 class="carbon-icon" />
+        <ChevronRight16 />
       {/if}
     </IconButton>
   </div>
@@ -263,7 +264,6 @@
       max-width: 15px;
     }
   }
-
   .footer {
     border-top: 1px solid var(--border-color);
     height: 50px;
