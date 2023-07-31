cb27a46999bcbc09f6a1e7674a6947c49767cb64
emmanuel <ifeanyi@digdippa.com>
2023-07-31 14:38:13 +0100

    CLASSROOMIO-T-75:Made the certificates downloadable for students when unlocked by their tutor

---
+++ src/components/Course/components/Ceritficate/StudentCertificate/LockedCertificate.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { course } from '../../../store';
+</script>
+
+<section class="studentContainer flex items-center justify-center w-full">
+  <div class="flex flex-col items-center justify-center w-max h-full gap-2">
+    <img src={`./${$course.certificate_theme}.svg`} alt="Certificate" />
+    <p class="text-xl font-normal text-center">Certificates not available</p>
+    <p class="text-sm font-normal text-center">
+      Your instructor needs to unlock your certificate. <br /> If you need your certificate,
+      please contact your instructor.
+    </p>
+  </div>
+</section>
+
+<style>
+  .studentContainer {
+    height: 80vh;
+  }
+</style>
