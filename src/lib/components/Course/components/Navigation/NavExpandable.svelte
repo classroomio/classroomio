1894fad3d101ed37459aecd40adf71f99104d3f9
Rotimi Best <best@classroomio.com>
2023-08-03 10:38:32 +0100

    Course UI improvements

--- src/lib/components/Course/components/Navigation/NavExpandable.svelte
+++ src/lib/components/Course/components/Navigation/NavExpandable.svelte
@@ -1,12 +1,13 @@
 <script>
   import CaretSortDownIcon from 'carbon-icons-svelte/lib/CaretSortDown.svelte';
 
-  let isExpanded = true;
   export let handleClick = () => {};
   export let label = '';
-  export let hideSortIcon = false;
   export let isGroupActive = false;
   // export let subMenuItems = [];
+
+  let isExpanded = true;
+  let activeClass = 'bg-gray-200 dark:bg-gray-700';
 
   function onClick() {
     // isExpanded = !isExpanded;
@@ -14,9 +15,10 @@
   }
 </script>
 
-<div>
+<div class="">
   <button
-    class="item relative flex items-center {isGroupActive && 'active'} w-full"
+    class="item relative flex items-center py-3 px-4 ml-2 mb-1 rounded hover:bg-gray-200 dark:hover:bg-gray-500 {isGroupActive &&
+      activeClass} w-[95%]"
     tabindex="0"
     on:click={onClick}
   >
@@ -28,38 +30,3 @@
     </div>
   {/if}
 </div>
-
-<style>
-  :global(svg.carbon-icon-sort) {
-    position: absolute;
-    top: 8px;
-    left: 0;
-  }
-  :global(svg.carbon-icon-minimize) {
-    transform: rotate(270deg);
-    top: 13px;
-    left: -6px;
-  }
-
-  .active {
-    background-color: #cae2f9;
-    border-left: 3px solid var(--main-primary-color);
-  }
-
-  :global(body.dark) .active,
-  :global(body.dark) .item:hover {
-    background-color: rgba(107, 114, 128);
-  }
-
-  .active span {
-    padding-left: 1.3rem;
-  }
-
-  .item span {
-    padding: 12px 24px;
-  }
-
-  .item:hover {
-    background-color: #cae2f9;
-  }
-</style>
