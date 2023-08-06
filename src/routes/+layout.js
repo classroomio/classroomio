503c40c70ab9fe81c6ccff6fa1fec20460058bbc
Rotimi Best <best@classroomio.com>
2023-08-06 17:31:29 +0100

    Remove netlify app from redirect rule

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -3,7 +3,7 @@ import { redirect } from '@sveltejs/kit';
 export const load = ({ url }) => {
   console.log('url', url.hostname);
 
-  if (url.hostname === 'classroomio.com' || url.hostname === 'classroomio.netlify.app') {
+  if (url.hostname === 'classroomio.com') {
     throw redirect(301, 'https://about.classroomio.com');
   }
 };
