2f05ee617f0d35fac08a5b316f7dbf41a838797a
tunny17 <tunji@digdippa.com>
2023-08-10 12:50:53 +0100

    CLASSROOMIO-T-84: Unresolved Fix

--- src/lib/components/Course/components/Lesson/Materials/constants.js
+++ src/lib/components/Course/components/Lesson/Materials/constants.js
@@ -1,16 +1,21 @@
+import { Video, Notebook, PresentationFile } from 'carbon-icons-svelte';
+
 export const tabs = [
   {
     label: 'Note',
+    icon: Notebook,
     value: 1,
     badgeValue: 0
   },
   {
     label: 'Slide',
+    icon: PresentationFile,
     value: 2,
     badgeValue: 0
   },
   {
     label: 'Video',
+    icon: Video,
     value: 3,
     badgeValue: 0
   }
