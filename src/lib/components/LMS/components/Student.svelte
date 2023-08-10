08b8dbc0df5dc3a18dd6a8889a69f2c0a0b4f81a
tunny17 <tunji@digdippa.com>
2023-08-10 12:23:10 +0000

    CLASSROOMIO-T-85:Developed the Dahshboard UI for the student LMS
    CLASSROOMIO-T-85:Added the datepicker and also created the UI for when no course is in progress
    
    CLASSROOMIO-T-85:Changed the images to svg and moved them into the images folder in the static dir
    
    CLASSROOMIO-T-85:Developed the Dahsboard UI for the student LMS
    
    Merge branch 'main' of https://gitlab.com/thenextgreat/classroomio
    
    Merge branch 'main' of https://gitlab.com/thenextgreat/classroomio
    
    Merge branch 'main' of https://gitlab.com/thenextgreat/classroomio
    
    Merge branch 'main' of https://gitlab.com/thenextgreat/classroomio
    
    Merge branch 'main' of https://gitlab.com/thenextgreat/classroomio
    
    Merge branch 'main' of https://gitlab.com/thenextgreat/classroomio
    
    CLASSROOMIO-T-27: Review Section for Instructors on landing page
    
    CLASSROOMIO-T-27: Remove unused variable
    
    Merge branch 'CLASSROOMIO-T-27' of https://gitlab.com/thenextgreat/classroomio into CLASSROOMIO-T-27
    
    CLASSROOMIO-T-27: Complete reviews functionality
    
    CLASSROOMIO-T-27: Review Render Update
    
    CLASSROOMIO-T-27:
    - Resolved Feedbacks
    
    CLASSROOMIO-T-27: Fixed Ratings Data
    
    CLASSROOMIO-T-27:
    - Added Ratings Svg
    - Fixed avatar_url feedback
    
    CLASSROOMIO-T-27: Import Adjustment
    
    CLASSROOMIO-T-27: Code Review
    
    CLASSROOM-T-27: Added Add Review Functionality
    
    CLASSROOMIO-T-27:
    - Unresolved Review
    - Enhance Code Redability
    
    CLASSROOMIO-T-27: Completed the delete functionality
    
    CLASSROOMIO-T-27: Hide/Show functionality
    
    CLASSROOMIO-T-27: Double Quotation Update
    
    CLASSROOMIO-T-27: Testing toggle functinality
    
    CLASSROOMIO-T-27: Added the icons
    
    CLASSROOMIO-T-27: Completion of the hide/show functionality
    
    CLASSROOMIO-T-27:
    Display the store data on the review page for styling
    
    CLASSROOMIO-T-27:
    Styling of the review component & connecting it's data
    
    CLASSROOMIO-T-27:
    - Add ReviewsForm
    - Completion of the Reviews Form Structure/Styling
    
    CLASSROOMIO-T-27: Added a reviews modal
    
    CLASSROOMIO-T-27: Logic update for review section
    
    CLASSROOMIO-T-50: Logic for overlapping reviews
    
    CLASSROOMIO-T-27:
    - Completion of the structure & styling with mock data.
    - Addition of custom ratings assets
    
    CLASSROOMIO-T-27:
    - Review Section Code Structure
    - Import Profile Picture Assets
    
    
    Co-authored-by: Rotimi Best <rotimiibitoyeemma@gmail.com>
    Co-authored-by: Tunji Adeyemi <tunji@digdippa.com>
    Co-authored-by: tunny17 <tvnji17@gmail.com>
    
    
    Merge-request: CLASSROOMIO-MR-22
    Merged-by: Best Admin <best@digdippa.com>
    CLASSROOMIO-T-62: Dark Mode Bug Fix
    
    CLASSROOMIO-T-62: Delete Unused class
    
    CLASSROOMIO-T-62: Dark Mode Bug Fix
    
    
    Co-authored-by: Tunji Adeyemi <tunji@digdippa.com>
    
    
    Merge-request: CLASSROOMIO-MR-23
    Merged-by: Best Admin <best@digdippa.com>
    
    Co-authored-by: Ifeanyi Nwosu <ifeanyi@digdippa.com>
    Co-authored-by: Tunji Adeyemi <tunji@digdippa.com>
    Co-authored-by: tunny17 <tvnji17@gmail.com>
    
    
    Merge-request: CLASSROOMIO-MR-37
    Merged-by: Best Admin <best@digdippa.com>

---
+++ src/lib/components/LMS/components/Student.svelte
@@ -0,0 +1,80 @@
+<script lang="ts">
+  import { InlineCalendar, Datepicker } from 'svelte-calendar';
+  import PrimaryButton from '$lib/components/PrimaryButton/index.svelte';
+  import Box from '$lib/components/Box/index.svelte';
+  import { isMobile } from '$lib/utils/store/useMobile';
+  import { formatDate } from '$lib/utils/functions/routes/dashboard';
+  import { VARIANTS } from '$lib/components/PrimaryButton/constants';
+
+  let store;
+
+  const theme = {
+    calendar: {
+      width: '22em',
+      height: '100%',
+      shadow: '0px 0px 5px rgba(0, 0, 0, 0.25)',
+      colors: {
+        background: {
+          highlight: '#0F62FE'
+        }
+      },
+      font: {
+        regular: '0.7em',
+        large: '0.9em'
+      }
+    }
+  };
+
+  // just copy and paste
+</script>
+
+<section class="w-full h-full mt-2">
+  <p class="text-base font-semibold text-[#040F2D] pb-3 dark:text-white">Your Schedule</p>
+
+  <Box
+    className="p-5 border-2 border-[#EAEAEA] gap-5 rounded w-full h-fit xl:h-full flex flex-col justify-start"
+  >
+    <div class="w-full hidden xl:flex items-center justify-center">
+      <InlineCalendar bind:store {theme} />
+    </div>
+    {#if $isMobile}
+      <div class="header w-full flex items-center">
+        <Datepicker bind:store let:key let:send let:receive>
+          <button
+            class="text-lg font-bold text-blue-700 p-3 hover:bg-gray-300 rounded-md"
+            in:receive|local={{ key }}
+            out:send|local={{ key }}
+          >
+            {formatDate($store?.selected)}
+          </button>
+        </Datepicker>
+      </div>
+    {:else}
+      <p class="xl:hidden dark:text-white font-bold m-5">
+        {formatDate($store?.selected)}
+      </p>
+    {/if}
+    {#if formatDate($store?.selected) == 'Today'}
+      <span class="flex flex-col md:flex-row items-baseline justify-center gap-10 mt-5">
+        <p class="text-xs font-normal text-[#656565] dark:text-white">2:30pm</p>
+        <div class="flex flex-col items-start gap-4">
+          <span>
+            <p class="text-lg font-semibold text-[#262626] dark:text-white">
+              The essence of design collaboration
+            </p>
+            <a href="#" class="text-sm font-normal text-[#0233BD] dark:text-white"
+              >Figma Introduction</a
+            >
+          </span>
+          <PrimaryButton
+            label="Join Call"
+            variant={VARIANTS.OUTLINED}
+            className="bg-transparent outline-1 outline-[#0233BD] text-[#0233BD] rounded-md hover:bg-[blue]"
+          />
+        </div>
+      </span>
+    {:else}
+      <p>No Lesson on this day</p>
+    {/if}
+  </Box>
+</section>
