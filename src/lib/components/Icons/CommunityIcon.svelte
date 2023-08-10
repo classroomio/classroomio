b83a396991c718884e17b0d0a656a6c4bb0968c4
tunny17 <tunji@digdippa.com>
2023-08-10 12:23:10 +0000

    CLASSROOMIO-T-85:Developed the Dahshboard UI for the student LMS
    CLASSROOMIO-T-85:Added the datepicker and also created the UI for when no course is in progress
    
    CLASSROOMIO-T-85:Changed the images to svg and moved them into the images folder in the static dir
    
    CLASSROOMIO-T-85:Developed the Dahsboard UI for the student LMS
    
    Merge branch 'main' of https://gitlab.com/thenextgreat/classroomio
    
    Merge branch 'main' of https://gitlab.com/thenextgreat/classroomio
    
    Merge branch 'main' of https://gitlab.com/thenextgreat/classroomio
    
    Merge branch 'main' of https://gitlab.com/thenextgreat/classroomio
    
    Merge branch 'main' of https://gitlab.com/thenextgreat/classroomio
    
    Merge branch 'main' of https://gitlab.com/thenextgreat/classroomio
    
    CLASSROOMIO-T-27: Review Section for Instructors on landing page
    
    CLASSROOMIO-T-27: Remove unused variable
    
    Merge branch 'CLASSROOMIO-T-27' of https://gitlab.com/thenextgreat/classroomio into CLASSROOMIO-T-27
    
    CLASSROOMIO-T-27: Complete reviews functionality
    
    CLASSROOMIO-T-27: Review Render Update
    
    CLASSROOMIO-T-27:
    - Resolved Feedbacks
    
    CLASSROOMIO-T-27: Fixed Ratings Data
    
    CLASSROOMIO-T-27:
    - Added Ratings Svg
    - Fixed avatar_url feedback
    
    CLASSROOMIO-T-27: Import Adjustment
    
    CLASSROOMIO-T-27: Code Review
    
    CLASSROOM-T-27: Added Add Review Functionality
    
    CLASSROOMIO-T-27:
    - Unresolved Review
    - Enhance Code Redability
    
    CLASSROOMIO-T-27: Completed the delete functionality
    
    CLASSROOMIO-T-27: Hide/Show functionality
    
    CLASSROOMIO-T-27: Double Quotation Update
    
    CLASSROOMIO-T-27: Testing toggle functinality
    
    CLASSROOMIO-T-27: Added the icons
    
    CLASSROOMIO-T-27: Completion of the hide/show functionality
    
    CLASSROOMIO-T-27:
    Display the store data on the review page for styling
    
    CLASSROOMIO-T-27:
    Styling of the review component & connecting it's data
    
    CLASSROOMIO-T-27:
    - Add ReviewsForm
    - Completion of the Reviews Form Structure/Styling
    
    CLASSROOMIO-T-27: Added a reviews modal
    
    CLASSROOMIO-T-27: Logic update for review section
    
    CLASSROOMIO-T-50: Logic for overlapping reviews
    
    CLASSROOMIO-T-27:
    - Completion of the structure & styling with mock data.
    - Addition of custom ratings assets
    
    CLASSROOMIO-T-27:
    - Review Section Code Structure
    - Import Profile Picture Assets
    
    
    Co-authored-by: Rotimi Best <rotimiibitoyeemma@gmail.com>
    Co-authored-by: Tunji Adeyemi <tunji@digdippa.com>
    Co-authored-by: tunny17 <tvnji17@gmail.com>
    
    
    Merge-request: CLASSROOMIO-MR-22
    Merged-by: Best Admin <best@digdippa.com>
    CLASSROOMIO-T-62: Dark Mode Bug Fix
    
    CLASSROOMIO-T-62: Delete Unused class
    
    CLASSROOMIO-T-62: Dark Mode Bug Fix
    
    
    Co-authored-by: Tunji Adeyemi <tunji@digdippa.com>
    
    
    Merge-request: CLASSROOMIO-MR-23
    Merged-by: Best Admin <best@digdippa.com>
    
    Co-authored-by: Ifeanyi Nwosu <ifeanyi@digdippa.com>
    Co-authored-by: Tunji Adeyemi <tunji@digdippa.com>
    Co-authored-by: tunny17 <tvnji17@gmail.com>
    
    
    Merge-request: CLASSROOMIO-MR-37
    Merged-by: Best Admin <best@digdippa.com>

--- src/lib/components/Icons/CommunityIcon.svelte
+++ src/lib/components/Icons/CommunityIcon.svelte
@@ -2,7 +2,14 @@
   export let color = 'currentColor';
 </script>
 
-<svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+<svg
+  width="22"
+  height="22"
+  viewBox="0 0 22 22"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+  class="dark"
+>
   <path
     fill-rule="evenodd"
     clip-rule="evenodd"
