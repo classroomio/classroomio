15d28ba6326edbb47356803e993921b8b9895f36
Rotimi Best <best@classroomio.com>
2023-08-12 18:46:35 +0100

    CLASSROOMIO-T-105: Add try again + sentry replay

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [sveltekit()],
+  plugins: [sveltekit(), sentryVitePlugin({
+    org: "the-next-greats",
+    project: "classroomio"
+  })],
 });
