0ba6e92b0beb9cfab0ed6e312a32eb990c6c56c9
Rotimi Best <best@classroomio.com>
2023-08-13 09:17:36 +0100

    Add sentry auth token to vite

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -62,7 +62,7 @@
         // Set tracesSampleRate to 1.0 to capture 100%
         // of transactions for performance monitoring.
         // We recommend adjusting this value in production
-        tracesSampleRate: 1.0
+        tracesSampleRate: 0.5
       });
     }
   }
