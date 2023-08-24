70fc3571e242827c7efb364837f56b19e98b70d3
Rotimi Best <best@classroomio.com>
2023-08-24 09:40:22 +0100

    Sidebar styling

--- src/lib/components/Course/components/Navigation/NavExpandable.svelte
+++ src/lib/components/Course/components/Navigation/NavExpandable.svelte
@@ -24,7 +24,7 @@
 
 <div class="">
   <button
-    class="item relative flex items-center py-3 px-4 ml-2 mb-1 rounded hover:bg-gray-200 hover:text-black {isGroupActive &&
+    class="item relative flex items-center py-3 px-4 ml-2 mb-1 {NavClasses.item} {isGroupActive &&
       NavClasses.active} w-[95%]"
     tabindex="0"
     on:click={onClick}
--- src/lib/utils/constants/reusableClass.js
+++ src/lib/utils/constants/reusableClass.js
@@ -1,5 +1,6 @@
 export const NavClasses = {
   active:
-    'bg-gray-200 dark:bg-gray-300 text-black dark:text-black transition delay-150 duration-300 ease-in-out',
-  background: ''
+    'bg-gray-200 dark:bg-gray-700 hover:dark:text-white text-black dark:text-white transition delay-150 duration-300 ease-in-out',
+  background: '',
+  item: 'hover:bg-gray-200 dark:hover:bg-gray-700 hover:text-black hover:dark:text-white rounded'
 };
