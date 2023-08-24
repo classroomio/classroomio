55bc01a6f8fa918a8c4c18b714273ef06e0e96e0
emmanuel <ifeanyi@digdippa.com>
2023-08-24 14:22:34 +0000

    CLASSROOMIO-T-123:Implemented the settings tab in the LMS'
    CLASSROOMIO-T-123:changed the width value'
    
    CLASSROOMIO-T-123:Deleted unused Imports and also changed the width value'
    
    CLASSROOMIO-T-123:Implemented the settings tab in the LMS'
    
    
    Co-authored-by: Ifeanyi Nwosu <ifeanyi@digdippa.com>
    
    
    Merge-request: CLASSROOMIO-MR-54
    Merged-by: Best Admin <best@digdippa.com>

--- src/routes/lms/settings/+page.svelte
+++ src/routes/lms/settings/+page.svelte
@@ -1,1 +1,30 @@
-Settings Page
+<script>
+  import { page } from '$app/stores';
+  import { Tabs, Tab, TabContent } from 'carbon-components-svelte';
+  import Profile from '$lib/components/LMS/components/Profile.svelte';
+
+  let selected = 0;
+  const tabs = [
+    {
+      key: 0,
+      label: 'Profile',
+      tabKey: '',
+      href: $page.url.pathname
+    }
+  ];
+</script>
+
+<section class="max-w-6xl mx-auto">
+  <div class="m-2 md:m-5">
+    <Tabs autoWidth bind:selected>
+      {#each tabs as tab}
+        <Tab label={tab.label} href={tab.href} />
+      {/each}
+      <svelte:fragment slot="content">
+        <TabContent>
+          <Profile />
+        </TabContent>
+      </svelte:fragment>
+    </Tabs>
+  </div>
+</section>
