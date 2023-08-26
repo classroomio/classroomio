029f902198dc681b13cab7c79ec91e143449dead
Rotimi Best <rotimiibitoyeemma@gmail.com>
2023-08-26 11:58:57 +0000

    CLASSROOMIO-T-133: Send welcome email after onboarding
    Co-authored-by: Rotimi Best <rotimiibitoyeemma@gmail.com>
    
    
    Merge-request: CLASSROOMIO-MR-55
    Merged-by: Best Admin <best@digdippa.com>

---
+++ src/lib/utils/services/sendgrid.ts
@@ -0,0 +1,17 @@
+import * as SendGridService from '@sendgrid/mail';
+import type { MailService } from '@sendgrid/mail';
+import { SENDGRID_API_KEY } from '$env/static/private';
+
+export const SENDGRID_FROM = 'Rotimi Best <best@classroomio.com>';
+
+export let sendgrid: MailService;
+
+export const getSendgrid = (): MailService => {
+  if (sendgrid) return sendgrid;
+
+  SendGridService.setApiKey(SENDGRID_API_KEY);
+
+  sendgrid = SendGridService;
+
+  return sendgrid;
+};
---
+++ src/routes/api/email/welcome/+server.js
@@ -0,0 +1,50 @@
+import { render } from 'svelte-email';
+import { json } from '@sveltejs/kit';
+import WelcomeTemplate from './template.svelte';
+import { getSendgrid, SENDGRID_FROM } from '$lib/utils/services/sendgrid';
+import { getSupabase } from '$lib/utils/functions/supabase';
+
+const sendgrid = getSendgrid();
+const supabase = getSupabase();
+
+export async function POST({ request }) {
+  const { to, name } = await request.json();
+  const accessToken = request.headers.get('Authorization');
+  console.log('/POST api/email/welcome', to, name);
+
+  if (!to || !name) {
+    return json({ success: false, message: 'Name and To are required fields' }, { status: 400 });
+  }
+
+  let user;
+  try {
+    const { data } = await supabase.auth.getUser(accessToken);
+    user = data.user;
+  } catch (error) {
+    console.error(error);
+  }
+
+  console.log('user', user);
+  if (!user) {
+    return json({ success: false, message: 'Unauthenticated user' }, { status: 401 });
+  }
+
+  const options = {
+    from: SENDGRID_FROM,
+    to,
+    subject: 'Thank you so so so much for choosing ClassroomIO!',
+    html: render({
+      template: WelcomeTemplate,
+      props: {
+        name
+      }
+    })
+  };
+
+  sendgrid.send(options);
+
+  return json({
+    success: true,
+    message: 'Email sent'
+  });
+}
