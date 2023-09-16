19caedf39709abc60d72d1bb5539dd800c576f40
Rotimi Best <best@classroomio.com>
2023-09-16 23:11:28 +0100

    keep button style

--- src/lib/components/Icons/ArrowTopRight.svelte
+++ src/lib/components/Icons/ArrowTopRight.svelte
@@ -2,8 +2,8 @@
   <svg
     xmlns="http://www.w3.org/2000/svg"
     class="icon icon-tabler icon-tabler-arrow-up-right"
-    width="24"
-    height="24"
+    width="20"
+    height="20"
     viewBox="0 0 24 24"
     stroke-width="1.5"
     stroke="currentColor"
