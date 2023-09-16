4f29a1706df7a6741e48b738cf48e31d87ad2b91
Rotimi Best <best@classroomio.com>
2023-09-16 23:11:28 +0100

    keep button style

--- src/lib/components/PrimaryButton/index.svelte
+++ src/lib/components/PrimaryButton/index.svelte
@@ -22,8 +22,8 @@
     : 'cursor-pointer'} flex items-center h-auto {VARIANTS_CLASS[
     isLoading ? VARIANTS.OUTLINED : variant
   ]} {!disablePadding &&
-    'py-[0.60rem] px-6'} rounded-md {className} w-fit justify-center sm:w-auto {isLoading &&
-    'py-[0.60rem] border-primary-700'} {variant !== VARIANTS.TEXT &&
+    'py-[0.5rem] px-6'} rounded-md {className} w-fit justify-center sm:w-auto {isLoading &&
+    'py-[1rem] border-primary-700'} {variant !== VARIANTS.TEXT &&
     'hover:shadow-xl'} transition delay-150 duration-300 ease-in-out {variant !==
   VARIANTS.CONTAINED_WHITE
     ? 'dark:text-white'
