9871a4a920b62105db5d2a14f6c55b8e5de314e4
Rotimi Best <rotimiibitoyeemma@gmail.com>
2023-09-16 22:22:24 +0100

    Add visit landing page site button

---
+++ src/lib/components/Buttons/VisitOrgSite.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import PrimaryButton from '$lib/components/PrimaryButton/index.svelte';
+  import ArrowUpRightIcon from '$lib/components/Icons/ArrowTopRight.svelte';
+  import { currentOrg } from '$lib/utils/store/org';
+
+  export let className = '';
+</script>
+
+<a
+  href={`https://${$currentOrg.siteName}.classroomio.com`}
+  target="_blank"
+  class="{className} ml-2 hover:no-underline"
+>
+  <PrimaryButton>
+    View site
+    <ArrowUpRightIcon />
+  </PrimaryButton>
+</a>
