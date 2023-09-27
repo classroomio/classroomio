0fe6e2ab876b5709311f72e6e29305d9e11d0e9b
Rotimi Best <best@classroomio.com>
2023-09-27 11:26:04 +0100

    New org improvements

--- src/lib/components/OrgSelector/OrgSelectorItem.svelte
+++ src/lib/components/OrgSelector/OrgSelectorItem.svelte
@@ -15,7 +15,8 @@
 
 <div class="relative">
   <button
-    class="{disabled && 'opacity-25'} flex items-center w-[14rem] py-2 px-3 {hasDivider &&
+    class="{disabled &&
+      'opacity-25'} flex items-center text-start w-[14rem] py-2 px-3 {hasDivider &&
       'border-b border-gray-100'} {active && 'bg-primary-100 dark:bg-gray-500'}"
     on:click={onClick}
   >
