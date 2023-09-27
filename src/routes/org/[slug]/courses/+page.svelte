11958ae6f75b4a2ab4fffc268b4564a4b20e7273
Rotimi Best <best@classroomio.com>
2023-09-27 04:23:45 +0100

    Add sorting and filtering

--- src/routes/org/[slug]/courses/+page.svelte
+++ src/routes/org/[slug]/courses/+page.svelte
@@ -1,5 +1,6 @@
-<script>
+<script lang="ts">
   import { onMount } from 'svelte';
+  import { Search, Dropdown } from 'carbon-components-svelte';
   import { page } from '$app/stores';
   import { profile } from '$lib/utils/store/user';
   import { fetchCourses } from '$lib/components/Courses/api';
@@ -11,9 +12,14 @@
   import { Add } from 'carbon-icons-svelte';
   import { isMobile } from '$lib/utils/store/useMobile';
   import { isOrgAdmin } from '$lib/utils/store/org';
+  import type { Course } from '$lib/utils/types';
+  import { browser } from '$app/environment';
 
   export let data;
   let { allCourses, cantFetch } = data;
+  let searchValue = '';
+  let selectedId: string;
+  let filteredCourses: Course[];
 
   const urlParams = new URLSearchParams($page.url.search);
 
@@ -21,7 +27,7 @@
     $createCourseModal.open = true;
   }
 
-  async function getCourses(userId, orgId) {
+  async function getCourses(userId: string | null, orgId: string) {
     if (cantFetch && typeof cantFetch === 'boolean' && !allCourses.length && orgId) {
       $courseMetaDeta.isLoading = true;
 
@@ -36,10 +42,41 @@
     }
   }
 
+  function filterCourses(searchValue: string, _selectedId: string, courses: Course[]) {
+    if (browser) {
+      if (!selectedId) {
+        selectedId = localStorage.getItem('classroomio_filter_course_key') || '0';
+      } else {
+        localStorage.setItem('classroomio_filter_course_key', _selectedId);
+      }
+    }
+
+    filteredCourses = courses.filter((course) => {
+      if (!searchValue || course.title.toLowerCase().includes(searchValue)) {
+        return true;
+      }
+
+      return false;
+    });
+
+    if (_selectedId === '0') {
+      filteredCourses = filteredCourses.sort(
+        (a, b) => new Date(a.created_at).getTime() - new Date(b.created_at).getTime()
+      );
+    } else if (_selectedId === '1') {
+      filteredCourses = filteredCourses.sort((a, b) => b.is_published - a.is_published);
+    } else if (_selectedId === '2') {
+      filteredCourses = filteredCourses.sort((a, b) => b.total_lessons - a.total_lessons);
+    }
+
+    console.log('filteredCourses', filteredCourses);
+  }
+
   onMount(() => {
     courses.set(allCourses);
   });
 
+  $: filterCourses(searchValue, selectedId, $courses);
   $: getCourses($profile.id, $currentOrg.id);
 </script>
 
@@ -49,7 +86,7 @@
 
 <section class="w-full md:max-w-6xl md:mx-auto">
   <div class="py-2 md:py-10 px-2 md:px-5">
-    <div class="flex items-center justify-between mb-10">
+    <div class="flex items-center justify-between mb-5">
       <h1 class="dark:text-white text-2xl md:text-3xl font-bold">Courses</h1>
       {#if $isMobile}
         <PrimaryButton isDisabled={!$isOrgAdmin} onClick={() => ($createCourseModal.open = true)}>
@@ -63,9 +100,35 @@
         />
       {/if}
     </div>
+    <div class="flex flex-row-reverse mb-5">
+      <div class="flex items-end justify-start">
+        <Search
+          placeholder="Find course"
+          bind:value={searchValue}
+          searchClass="mr-2"
+          class=" bg-gray-100 dark:bg-neutral-800"
+        />
+        <Dropdown
+          class="h-full"
+          bind:selectedId
+          items={[
+            { id: '0', text: 'Date Created' },
+            { id: '1', text: 'Published' },
+            { id: '2', text: 'Lessons' }
+          ]}
+        />
+      </div>
+    </div>
 
     <NewCourseModal />
 
-    <Courses />
+    <Courses bind:courses={filteredCourses} />
   </div>
 </section>
+
+<style>
+  :global(.bx--dropdown) {
+    max-height: unset;
+    height: 100%;
+  }
+</style>
