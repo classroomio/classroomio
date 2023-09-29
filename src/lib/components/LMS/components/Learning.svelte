8777ab57f9a2bc87f4af592ae28e03a51c872e95
Rotimi Best <best@classroomio.com>
2023-09-29 09:25:32 +0100

    CLASSROOMIO-T-180: LMS dashboard improvements

--- src/lib/components/LMS/components/Learning.svelte
+++ src/lib/components/LMS/components/Learning.svelte
@@ -1,14 +1,13 @@
-<script>
+<script lang="ts">
   import { VARIANTS } from '$lib/components/PrimaryButton/constants';
   import { goto } from '$app/navigation';
   import PrimaryButton from '$lib/components/PrimaryButton/index.svelte';
-  import { course } from '$lib/components/Course/store';
-  import Box from '$lib/components/Box/index.svelte';
   import CoursesEmptyIcon from '$lib/components/Icons/CoursesEmptyIcon.svelte';
-  import { courseInProgress } from './store';
+  import { courseInProgress } from '$lib/components/Courses/store';
 
-  const gotoCourse = (title) => {
-    goto(`/courses/${title}`);
+  const gotoCourse = (id: string | undefined) => {
+    if (!id) return;
+    goto(`/courses/${id}`);
   };
 </script>
 
@@ -17,27 +16,34 @@
   <div
     class="flex items-center flex-col border border-[#EAEAEA] dark:bg-neutral-800 gap-2 rounded w-full h-fit xl:h-[516px] p-3"
   >
-    {#if courseInProgress.length > 0}
+    {#if $courseInProgress.length > 0}
       <div class="w-full h-full xl:h-[516px] flex flex-col justify-start">
-        {#each courseInProgress as data, index}
+        {#each $courseInProgress as course}
           <div class="p-5">
             <span class="flex flex-col md:flex-row gap-3 items-start pb-5">
-              <img src={data.src} alt="course" class="hidden md:block md:w-[60px] md:h-[60px]" />
+              <img
+                src={course.logo || '/images/classroomio-course-img-template.jpg'}
+                alt="course"
+                class="hidden md:block md:w-[60px] md:h-[60px]"
+              />
               <div class="w-full">
-                <p class="text-base font-semibold dark:text-white">{data.title}</p>
+                <p class="text-base font-semibold dark:text-white">{course.title}</p>
                 <p class="line-clamp-2 text-xs font-normal text-[#656565] dark:text-white">
-                  {data.description}
+                  {course.description}
                 </p>
               </div>
               <PrimaryButton
                 label="Continue"
                 variant={VARIANTS.OUTLINED}
                 className="rounded-none text-[#0233BD]"
-                onClick={() => gotoCourse(data.title)}
+                onClick={() => gotoCourse(course.id)}
               />
             </span>
             <div class="relative bg-[#EAEAEA] w-full h-1">
-              <div class={`absolute top-0 left-0 bg-primary-700 h-full ${data.progress} ]`} />
+              <div
+                style="width:{course.progress_rate}%"
+                class={`absolute top-0 left-0 bg-primary-700 h-full`}
+              />
             </div>
           </div>
         {/each}
@@ -45,7 +51,7 @@
     {:else}
       <div class="flex flex-col items-center">
         <CoursesEmptyIcon />
-        <h3 class="text-center">No courses progress yet</h3>
+        <h3 class="text-center">No courses in progress yet</h3>
         <p class="w-[75%] text-center">Once you start a course, your progress will reflect here</p>
       </div>
     {/if}
