1ac39cf49db51195e4e5b2a3f207e39770c1107c
emmanuel <ifeanyi@digdippa.com>
2023-10-31 10:08:23 +0000

    CLASSROOMIO-T-246:Working on the Anouncement page

---
+++ src/lib/components/Course/components/AnoucementCard.svelte
@@ -0,0 +1,42 @@
+<script>
+  import OverflowMenuVertical from 'carbon-icons-svelte/lib/OverflowMenuVertical.svelte';
+  import Send from 'carbon-icons-svelte/lib/Send.svelte';
+  export let value = {};
+</script>
+
+<div class="flex flex-col gap-5 border-2 border-gray-200 rounded-md my-5">
+  <section class="p-3">
+    <div class="flex justify-between mb-2">
+      <span class="flex items-center gap-3">
+        <div class="w-9 h-9">
+          <img
+            src={value.image}
+            alt="users banner"
+            class="w-full h-full rounded-full object-cover"
+          />
+        </div>
+        <span>
+          <p class="text-base font-semibold">{value.name}</p>
+          <p class="text-sm font-medium text-gray-600">08:41</p>
+        </span>
+      </span>
+      <OverflowMenuVertical size={24} />
+    </div>
+    <p class="text-sm font-medium w-[80%]">{value.content}</p>
+  </section>
+  <section class="flex items-center justify-between gap-2 border-t-2 border-gray-200 p-3">
+    <div class="w-7 h-7">
+      <img src={value.image} alt="users banner" class="w-full h-full rounded-full object-cover" />
+    </div>
+    <div class="flex-1">
+      <input
+        type="text"
+        name=""
+        id=""
+        placeholder="Add class comment"
+        class="w-full bg-transparent border-2 border-gray-200 rounded-3xl"
+      />
+    </div>
+    <Send size={24} />
+  </section>
+</div>
--- src/lib/components/Course/index.svelte
+++ src/lib/components/Course/index.svelte
@@ -8,9 +8,37 @@
   import { updateCourse } from '$lib/utils/services/courses';
   import TextEditor from '$lib/components/TextEditor/index.svelte';
   import HtmlRender from '$lib/components/HTMLRender/HTMLRender.svelte';
-
+  import AnoucementCard from './components/AnoucementCard.svelte';
+  import { Dropdown } from 'carbon-components-svelte';
+  import LogoYoutube from 'carbon-icons-svelte/lib/LogoYoutube.svelte';
+  import { Link } from 'carbon-icons-svelte';
+  import Upload from 'carbon-icons-svelte/lib/Upload.svelte';
+  import IconButton from '$lib/components/IconButton/index.svelte';
   let mode = MODES.view;
   let isDirty = false;
+  const mockAnouncements = [
+    {
+      id: 1,
+      image:
+        'https://www.befunky.com/images/prismic/82e0e255-17f9-41e0-85f1-210163b0ea34_hero-blur-image-3.jpg?auto=avif,webp&format=jpg&width=896',
+      content: 'this is a demo anounceent made to show tell you what is happening',
+      name: 'Best Emmanuel Ibitoye-Rotimi'
+    },
+    {
+      id: 2,
+      image:
+        'https://www.befunky.com/images/prismic/82e0e255-17f9-41e0-85f1-210163b0ea34_hero-blur-image-3.jpg?auto=avif,webp&format=jpg&width=896',
+      content: 'this is a demo anounceent made to show tell you what is happening',
+      name: 'Best Emmanuel Ibitoye-Rotimi'
+    },
+    {
+      id: 3,
+      image:
+        'https://www.befunky.com/images/prismic/82e0e255-17f9-41e0-85f1-210163b0ea34_hero-blur-image-3.jpg?auto=avif,webp&format=jpg&width=896',
+      content: 'this is a demo anounceent made to show tell you what is happening',
+      name: 'Best Emmanuel Ibitoye-Rotimi'
+    }
+  ];
 
   async function handleModeChange() {
     if (mode === MODES.edit) {
@@ -25,32 +53,58 @@
   }
 </script>
 
-<PageNav title="Overview" disableSticky={true}>
-  <svelte:fragment slot="widget">
-    <RoleBasedSecurity allowedRoles={[1, 2]}>
-      <div class="flex items-center">
-        <PrimaryButton
-          className="mr-2"
-          label={mode === MODES.edit ? 'Save' : 'Edit'}
-          onClick={handleModeChange}
-        />
-      </div>
-    </RoleBasedSecurity>
-  </svelte:fragment>
-</PageNav>
+<PageNav title="Anouncements" disableSticky={true} />
 
 <PageBody width="max-w-3xl px-3">
-  {#if mode === MODES.edit}
+  <section class="flex flex-col border-2 rounded-xl p-3">
+    <div class="mb-3">
+      <p class="mb-3 text-base font-medium">For</p>
+      <section class="flex gap-2 mb-2">
+        <Dropdown
+          selectedId="0"
+          items={[
+            { id: '0', text: 'Authors' },
+            { id: '1', text: 'Tutors' }
+          ]}
+        />
+        <Dropdown
+          selectedId="0"
+          items={[
+            { id: '0', text: 'Students' },
+            { id: '1', text: 'Visitors' },
+            { id: '2', text: 'Public' }
+          ]}
+        />
+      </section>
+    </div>
     <TextEditor
-      value={$course.overview}
       onChange={(html) => {
         isDirty = true;
         $course.overview = html;
       }}
-      placeholder="Write your overview here"
-      height="700"
+      placeholder="Make an anouncement to your students"
+      maxHeight={200}
     />
-  {:else}
-    <HtmlRender className="p-2" content={$course.overview || ''} />
-  {/if}
+    <div class="flex items-center justify-between py-2">
+      <div class="flex gap-2">
+        <IconButton contained={true}>
+          <LogoYoutube size={20} />
+        </IconButton>
+        <IconButton contained={true}>
+          <Link size={20} />
+        </IconButton>
+        <IconButton contained={true}>
+          <Upload size={20} />
+        </IconButton>
+      </div>
+      <div class="flex gap-2">
+        <PrimaryButton label="Cancel" />
+        <PrimaryButton label="Post" />
+      </div>
+    </div>
+  </section>
+  {#each mockAnouncements as info}
+    <AnoucementCard value={info} />
+  {/each}
+  <!-- <HtmlRender className="p-2" content={$course.overview || ''} /> -->
 </PageBody>
