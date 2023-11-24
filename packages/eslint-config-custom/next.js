e347157467bacb27580573132bce71a8e9137de4
elvis-ndubuisi <provictor.ie@gmail.com>
2023-11-24 09:02:03 +0100

    chore: add shared eslint config package

---
+++ packages/eslint-config-custom/next.js
@@ -0,0 +1,33 @@
+const { resolve } = require("node:path");
+
+const project = resolve(process.cwd(), "tsconfig.json");
+
+module.exports = {
+	extends: [
+		"@vercel/style-guide/eslint/node",
+		"@vercel/style-guide/eslint/browser",
+		"@vercel/style-guide/eslint/typescript",
+		"@vercel/style-guide/eslint/react",
+		"@vercel/style-guide/eslint/next",
+		"eslint-config-turbo",
+	].map(require.resolve),
+	parserOptions: {
+		project,
+	},
+	globals: {
+		React: true,
+		JSX: true,
+	},
+	settings: {
+		"import/resolver": {
+			typescript: {
+				project,
+			},
+		},
+	},
+	ignorePatterns: ["node_modules/", "dist/"],
+	// add rules configurations here
+	rules: {
+		"import/no-default-export": "off",
+	},
+};
