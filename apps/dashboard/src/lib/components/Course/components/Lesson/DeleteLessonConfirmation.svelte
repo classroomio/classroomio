fd4313746fe69ea44a94d69136eb9c772fc54970
Rotimi Best <best@classroomio.com>
2023-12-03 18:05:11 +0300

    feat: implement delete confirmation on lesson

---
+++ apps/dashboard/src/lib/components/Course/components/Lesson/DeleteLessonConfirmation.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  import Modal from '$lib/components/Modal/index.svelte';
+  import PrimaryButton from '$lib/components/PrimaryButton/index.svelte';
+
+  import { VARIANTS } from '$lib/components/PrimaryButton/constants';
+
+  export let lessonTitle: string;
+  export let openDeleteModal = false;
+  export let deleteLesson = () => {};
+
+  async function handleDelete() {
+    deleteLesson();
+    openDeleteModal = false;
+  }
+</script>
+
+<Modal
+  onClose={() => (openDeleteModal = false)}
+  bind:open={openDeleteModal}
+  width="w-96"
+  modalHeading="Delete"
+>
+  <div>
+    <h1 class="dark:text-white text-lg">
+      Are you sure you want to delete: <strong>{lessonTitle}</strong>?
+    </h1>
+
+    <div class="mt-5 flex items-center justify-between">
+      <PrimaryButton
+        className="px-6 py-3"
+        variant={VARIANTS.OUTLINED}
+        label="No"
+        onClick={() => (openDeleteModal = false)}
+      />
+      <PrimaryButton
+        className="px-6 py-3"
+        variant={VARIANTS.OUTLINED}
+        label="Yes"
+        onClick={handleDelete}
+      />
+    </div>
+  </div>
+</Modal>
