845b87b21f49e11738a0f38540d8432cc295f72f
Rotimi Best <best@classroomio.com>
2023-12-14 22:41:16 +0300

    chore: update posthug and change all 301 to 307

--- apps/classroomio-com/src/routes/github/+page.server.ts
+++ apps/classroomio-com/src/routes/github/+page.server.ts
@@ -3,10 +3,9 @@ import { client } from '$lib/utils/posthog';
 
 export const load = async ({ request }) => {
   client.capture({
-    distinctId:
-      request.headers.get('x-forwarded-for') || new Date().getTime().toString(),
-    event: 'github page visited',
+    distinctId: request.headers.get('x-forwarded-for') || new Date().getTime().toString(),
+    event: 'github page visited'
   });
 
-  throw redirect(301, 'https://github.com/rotimi-best/classroomio');
+  throw redirect(307, 'https://github.com/rotimi-best/classroomio');
 };
