4082b0aeca8846196488d79d8f7eb210787e9c80
emmanuel <chifez1@gmail.com>
2023-12-14 14:13:17 +0000

    style: Improved font size of the answers in the FAQ section of the landing page

--- apps/classroomio-com/src/lib/Faq/Faq.svelte
+++ apps/classroomio-com/src/lib/Faq/Faq.svelte
@@ -4,35 +4,27 @@
 
 <div class="mt-[5%] mb-[15%]">
   <header class="text-center mb-[5%] px-[8%]">
-    <h1
-      class="mt-2 text-4xl lg:text-4xl font-medium tracking-tight text-gray-900"
-    >
+    <h1 class="mt-2 text-4xl lg:text-4xl font-medium tracking-tight text-gray-900">
       Questions & Answers
     </h1>
     <h3
       class="lg:px-[20%] mt-4 text-lg lg:text-xl text-gray-500 inter font-normal tracking-normal lg:tracking-wide"
     >
       Can’t find the answer you’re looking for?
-      <a
-        class="text-[#1D4EE2]"
-        href="mailto:help@classroomio.com"
-        target="_blank">Shoot us an email</a
+      <a class="text-[#1D4EE2]" href="mailto:help@classroomio.com" target="_blank"
+        >Shoot us an email</a
       > and we’ll get back to you ASAP.
     </h3>
   </header>
 
-  <div
-    class="grid grid-cols-1 lg:grid-cols-3 gap-10 lg:gap-20 lg:px-[13%] mt-[20%] lg:m-0"
-  >
+  <div class="grid grid-cols-1 lg:grid-cols-3 gap-10 lg:gap-20 lg:px-[13%] mt-[20%] lg:m-0">
     {#each faqs as faq}
       <div class="max-w-[100%] px-6 lg:p-0">
-        <h1
-          class="text:md lg:text-md font-semibold leading-8 text-gray-800 inter"
-        >
+        <h1 class="text:md lg:text-md font-semibold leading-8 text-gray-800 inter">
           {faq.question}
         </h1>
         <p
-          class="mt-2 text:md lg:text-md font-thin leading-7 lg:leading-9 text-gray-600 inter tracking-wide"
+          class="mt-2 text:md lg:text-md font-normal leading-7 lg:leading-9 text-gray-600 inter tracking-wide"
         >
           {faq.answer}
         </p>
@@ -43,9 +35,7 @@
   <div
     class="floating relative mt-[10%] grad w-[100%] h-[61vh] lg:h-[54vh] flex items-center flex-col justify-center lg:w-[70%] mx-auto py-[12%] px-[6%] text-center shadow-2xl lg:rounded-3xl"
   >
-    <h1
-      class="mt-3 lg:mt-2 text-3xl lg:text-4xl font-bold font-display tracking-tight text-white"
-    >
+    <h1 class="mt-3 lg:mt-2 text-3xl lg:text-4xl font-bold font-display tracking-tight text-white">
       Supercharge your Classroom Today
     </h1>
     <p class="mt-6 text-xl lg:text-lg text-gray-300 inter">
@@ -64,11 +54,7 @@
 <style>
   .grad {
     background: rgb(51, 16, 71);
-    background: radial-gradient(
-      circle at 50% 100%,
-      rgba(51, 16, 71, 1) 1%,
-      rgba(2, 0, 36, 1) 100%
-    );
+    background: radial-gradient(circle at 50% 100%, rgba(51, 16, 71, 1) 1%, rgba(2, 0, 36, 1) 100%);
     background-size: cover;
   }
 </style>
