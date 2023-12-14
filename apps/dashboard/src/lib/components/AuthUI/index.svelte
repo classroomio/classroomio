126bdb4cc8627b0946206966dbe399f672c95c0c
Rotimi Best <best@classroomio.com>
2023-12-14 11:44:49 +0300

    debug: proper redirect

--- apps/dashboard/src/lib/components/AuthUI/index.svelte
+++ apps/dashboard/src/lib/components/AuthUI/index.svelte
@@ -20,10 +20,20 @@
     if (isLoading) {
       return;
     }
+
+    const params = new URLSearchParams(window.location.search);
+    console.log({ params });
+    const redirectTo = window.location.origin + params.get('redirectTo');
+
+    console.log({ redirectTo });
+
     try {
       console.log('signInWithGoogle');
       const { error, data } = await supabase.auth.signInWithOAuth({
-        provider: 'google'
+        provider: 'google',
+        options: {
+          redirectTo
+        }
       });
 
       console.log('data', data);
