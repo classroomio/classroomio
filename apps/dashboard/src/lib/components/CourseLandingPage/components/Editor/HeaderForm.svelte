a8a7d19e61df270f23a05be4e4fb9d1b6fd25353
rahbhee <ahmedrabi2003@gmail.com>
2023-12-17 01:06:27 +0100

    added replace button logic

--- apps/dashboard/src/lib/components/CourseLandingPage/components/Editor/HeaderForm.svelte
+++ apps/dashboard/src/lib/components/CourseLandingPage/components/Editor/HeaderForm.svelte
@@ -4,12 +4,48 @@
   import { VARIANTS } from '$lib/components/PrimaryButton/constants';
   import PrimaryButton from '$lib/components/PrimaryButton/index.svelte';
   import { handleOpenWidget } from '../../store';
+  import { onMount } from 'svelte';
 
   export let course = {};
-
+  
   function widgetControl() {
     $handleOpenWidget.open = true;
   }
+  
+  function widgetControl() {
+    $handleOpenWidget.open = true;
+    console.log('Opening widget...');
+  }
+
+///////////////////////////////////////////////////////////
+
+<!-- Here is my suggested implementation of the replace button logic:
+
+//Here is an array of fake images(i couldn't find the folder where the images are)
+
+const imageArray = [
+    'https://example.com/image1.png',
+    'https://example.com/image2.png',
+    'https://example.com/image3.png',
+    // Add more image URLs as needed
+  ];
+
+  let currentImageIndex = 0;
+  $: currentImageUrl = imageArray[currentImageIndex];
+
+  function replaceCoverImage() {
+    // Increment of the index or reset to 0 if it exceeds the array length
+    currentImageIndex = (currentImageIndex + 1) % imageArray.length;
+  }
+
+  //The component template will contain an <img> tag for the cover image (i couldnt locate this either):
+
+  <img bind:src={currentImageUrl} alt="Cover Image" />
+
+  -->
+
+//////////////////////////////////////////////////
+
 </script>
 
 <TextField className="mt-5" labelClassName="font-bold" label="Title" bind:value={course.title} />
