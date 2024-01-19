a1dcc0e85aa279cda4d527fe63b17fc29c41d94b
Rotimi Best <best@classroomio.com>
2024-01-19 16:40:52 +0100

    refactor: remove unused supabase storage path (#129)

--- apps/dashboard/src/lib/utils/functions/supabase.ts
+++ apps/dashboard/src/lib/utils/functions/supabase.ts
@@ -1,8 +1,4 @@
-import {
-  PUBLIC_SUPABASE_ANON_KEY,
-  PUBLIC_SUPABASE_URL,
-  PUBLIC_SUPABASE_BUCKET_URL
-} from '$env/static/public';
+import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public';
 import { createClient, SupabaseClient } from '@supabase/supabase-js';
 import type { ConfigType } from '../types';
 
@@ -12,7 +8,6 @@ export const getSupabase = (config?: ConfigType) => {
   if (supabase) return supabase;
   config = {
     supabaseConfig: {
-      bucketPath: PUBLIC_SUPABASE_BUCKET_URL + '/storage/v1/object/sign/',
       url: PUBLIC_SUPABASE_URL || '',
       anonKey: PUBLIC_SUPABASE_ANON_KEY || ''
     }
