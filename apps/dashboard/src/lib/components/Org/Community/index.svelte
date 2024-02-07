c65a02d40da2479c2110be89d667558bef9bb443
tunny17 <tvnji01@gmail.com>
2024-02-08 00:49:15 +0100

    feat: course dropdown

--- apps/dashboard/src/lib/components/Org/Community/index.svelte
+++ apps/dashboard/src/lib/components/Org/Community/index.svelte
@@ -9,55 +9,11 @@
   import { currentOrgPath, currentOrg } from '$lib/utils/store/org';
   import { supabase } from '$lib/utils/functions/supabase';
   import { calDateDiff } from '$lib/utils/functions/date';
-  import { course } from '$lib/components/Course/store';
 
   export let isLMS = false;
 
   let isLoading = false;
   let discussions = [];
-  let lmsDiscussions = [];
-
-  // async function fetchCommunityQuestionsForLMS(orgId?: string, currentCourseId?: string) {
-  //   if (!orgId) return;
-  //   isLoading = true;
-  //   const { data, error } = await supabase
-  //     .from('community_question')
-  //     .select(
-  //       `
-  //       organization_id,
-  //       course_id,
-  //       title,
-  //       votes,
-  //       created_at,
-  //       slug,
-  //       comments:community_answer(count),
-  //       author:profile(
-  //         fullname
-  //       )
-  //     `
-  //     )
-  //     .eq('organization_id', orgId)
-  //     .eq('course_id', currentCourseId)
-  //     .order('created_at', { ascending: false });
-  //   console.log('data', data);
-  //   console.log('error', error);
-  //   isLoading = false;
-
-  //   if (error) {
-  //     console.error('Error loading community', error);
-  //     // return goto(isLMS ? '/lms' : $currentOrgPath);
-  //   }
-
-  //   lmsDiscussions =
-  //     data?.map((lmsDiscussion) => ({
-  //       title: lmsDiscussion.title,
-  //       slug: lmsDiscussion.slug,
-  //       author: lmsDiscussion?.author?.fullname,
-  //       comments: lmsDiscussion.comments?.[0]?.count || 0,
-  //       votes: lmsDiscussion.votes,
-  //       createdAt: calDateDiff(lmsDiscussion.created_at)
-  //     })) || [];
-  // }
 
   async function fetchCommunityQuestions(orgId?: string) {
     if (!orgId) return;
@@ -67,6 +23,7 @@
       .select(
         `
         organization_id,
+        course_id,
         title,
         votes,
         created_at,
@@ -74,6 +31,9 @@
         comments:community_answer(count),
         author:profile(
           fullname
+        ),
+        course_title:course(
+          title
         )
       `
       )
@@ -81,6 +41,8 @@
       .order('created_at', { ascending: false });
     console.log('data', data);
     console.log('error', error);
+    console.log();
+
     isLoading = false;
 
     if (error) {
@@ -91,6 +53,8 @@
     discussions =
       data?.map((discussion) => ({
         title: discussion.title,
+        course_id: discussion.course_id,
+        course_title: discussion.course_title.title,
         slug: discussion.slug,
         author: discussion?.author?.fullname,
         comments: discussion.comments?.[0]?.count || 0,
@@ -100,7 +64,6 @@
   }
 
   $: fetchCommunityQuestions($currentOrg.id);
-  // $: fetchCommunityQuestionsForLMS($currentOrg.id, $course.id);
 </script>
 
 <div
@@ -111,41 +74,11 @@
     <CommunityLoader />
     <CommunityLoader />
     <CommunityLoader />
-  {:else if isLMS}
-    {#each lmsDiscussions as discussion}
-      <div class="w-full flex border-bottom-c p-5">
-        <Vote value={discussion.votes} />
-        <div class="discussion-topic-author">
-          <h4>
-            <a
-              class="text-black dark:text-white"
-              href="{isLMS ? '/lms' : $currentOrgPath}/community/{discussion.slug}"
-            >
-              {discussion.title}
-            </a>
-          </h4>
-          <span class="text-gray-600 dark:text-white">
-            {discussion.author} asked {discussion.createdAt}
-          </span>
-        </div>
-        <Space />
-        <div class="flex items-center">
-          <AddCommentIcon size={20} />
-          <span class="ml-1">{discussion.comments}</span>
-        </div>
-      </div>
-    {:else}
-      <Box className="w-screen">
-        <CoursesEmptyIcon />
-        <h3 class="dark:text-white text-2xl my-5">No Questions asked</h3>
-        <p class="dark:text-white w-1/3 text-center">Ask a question to the community</p>
-      </Box>
-    {/each}
   {:else}
     {#each discussions as discussion}
       <div class="w-full flex border-bottom-c p-5">
         <Vote value={discussion.votes} />
-        <div class="discussion-topic-author">
+        <div class="discussion-topic-author flex flex-col gap-y-0.5">
           <h4>
             <a
               class="text-black dark:text-white"
@@ -157,6 +90,14 @@
           <span class="text-gray-600 dark:text-white">
             {discussion.author} asked {discussion.createdAt}
           </span>
+          <a
+            class="text-gray-600 dark:text-white text-xs"
+            href={isLMS
+              ? `/lms/courses/${discussion.course_id}`
+              : `/courses/${discussion.course_id}`}
+          >
+            {discussion.course_title}
+          </a>
         </div>
         <Space />
         <div class="flex items-center">
