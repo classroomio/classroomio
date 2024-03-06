03e47d9aee28d7c6274c4a5e4152edf5392d506f
Rotimi Best <best@classroomio.com>
2024-03-06 22:36:49 +0100

    fix: student course payment 4010 error

--- apps/dashboard/src/routes/api/email/course/student_prove_payment/+server.js
+++ apps/dashboard/src/routes/api/email/course/student_prove_payment/+server.js
@@ -1,12 +1,12 @@
 import { json } from '@sveltejs/kit';
-import { getSupabase } from '$lib/utils/functions/supabase';
+// import { getSupabase } from '$lib/utils/functions/supabase';
 import sendEmail from '$defer/sendEmail';
 
-const supabase = getSupabase();
+// const supabase = getSupabase();
 
 export async function POST({ request }) {
   const { to, courseName, teacherEmail, studentFullname, orgName } = await request.json();
-  const accessToken = request.headers.get('Authorization') || '';
+  // const accessToken = request.headers.get('Authorization') || '';
   console.log(
     '/POST api/email/course/student_prove_payment',
     to,
@@ -20,17 +20,17 @@ export async function POST({ request }) {
     return json({ success: false, message: 'Missing required fields' }, { status: 400 });
   }
 
-  let user;
-  try {
-    const { data } = await supabase.auth.getUser(accessToken);
-    user = data.user;
-  } catch (error) {
-    console.error(error);
-  }
+  // let user;
+  // try {
+  //   const { data } = await supabase.auth.getUser(accessToken);
+  //   user = data.user;
+  // } catch (error) {
+  //   console.error(error);
+  // }
 
-  if (!user) {
-    return json({ success: false, message: 'Unauthenticated user' }, { status: 401 });
-  }
+  // if (!user) {
+  //   return json({ success: false, message: 'Unauthenticated user' }, { status: 401 });
+  // }
 
   await sendEmail({
     from: `"${orgName} - ClassroomIO" <notify@classroomio.com>`,
@@ -39,8 +39,8 @@ export async function POST({ request }) {
     subject: `[${courseName}] One more step left`,
     content: `
     <p>Hi ${studentFullname},</p>
-      <p>You are one step closer to joining: ${courseName}</p>
-      <p>Please send your proof of payment to: ${teacherEmail}, in order to join the course.</p>
+      <p>You are one step closer to joining: <strong>${courseName}</strong></p>
+      <p>Please send your proof of payment to: <strong>${teacherEmail}</strong>, in order to join the course.</p>
       <p>Talk to you soon and see you in class.</p>
       <p>${orgName}</p>
     `
