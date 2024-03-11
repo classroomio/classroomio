a851bd2fb6af63e03a85383193ecf6f1c1754f41
emmanuel <chifez1@gmail.com>
2024-03-11 16:17:14 +0100

    Updated the pricing plans

--- apps/dashboard/src/lib/components/Org/UpgradePlan/UpgradePlanCard.svelte
+++ apps/dashboard/src/lib/components/Org/UpgradePlan/UpgradePlanCard.svelte
@@ -8,22 +8,22 @@
     <div class="isolate grid grid-cols-1 lg:grid-cols-3">
       <!-- card 1 -->
       <div class="max-w-xl p-4 ring-1 ring-gray-200 lg:max-w-sm">
-        <p class="text-lg font-semibold leading-8 text-gray-900 lg:text-xl">Hobby</p>
+        <p class="text-lg font-semibold leading-8 text-gray-900 lg:text-xl">{PLANS.BASIC.NAME}</p>
         <p class="flex items-baseline gap-x-1 font-medium text-baseline">
-          USD ${PLANS.FREE.PRICE.MONTHLY} /month
+          USD ${PLANS.BASIC.PRICE.MONTHLY}
         </p>
         <p class=" mt-4 text-sm font-light leading-6 text-gray-500 lg:leading-6">
-          For personal use. Hobby plan is free for up to 5 courses forever, no credit card required.
+          {PLANS.BASIC.DESCRIPTION}
         </p>
-        <button
-          class="text-md mt-4 w-full rounded-md bg-slate-900 py-3 font-medium text-white hover:bg-slate-700 lg:rounded-md lg:py-3 lg:text-lg lg:font-semibold"
-          data-cal-config={"{'layout':'month_view'}"}
-          data-cal-link="classroomio/demo"
+        <a
+          class="block hover:no-underline text-center text-md mt-4 w-full rounded-md bg-slate-900 py-3 font-medium text-white hover:bg-slate-700 lg:rounded-md lg:py-3 lg:text-lg lg:font-semibold"
+          target="_blank"
+          href={PLANS.BASIC.CTA.LINK}
         >
-          Get started
-        </button>
+          {PLANS.BASIC.CTA.LABEL}
+        </a>
         <ul class="mt-4 space-y-2 text-sm text-gray-600">
-          {#each PLANS.FREE.FEATURES as freeFeature}
+          {#each PLANS.BASIC.FEATURES as freeFeature}
             <li class="flex items-center">
               <div>
                 <Checkmark size={16} fill="#1D4EE2" class="mr-2 lg:mr-3" />
@@ -38,22 +38,24 @@
 
       <!-- card 2 -->
       <div class="max-w-xl bg-gray-900 ring-1 ring-gray-200 p-4 lg:max-w-sm">
-        <p class="text-lg font-semibold leading-8 text-white lg:text-xl">Pro</p>
+        <p class="text-lg font-semibold leading-8 text-white lg:text-xl">
+          {PLANS.EARLY_ADOPTER.NAME}
+        </p>
         <p class="flex items-baseline gap-x-1 text-baseline text-white font-medium">
-          USD ${PLANS.PRO.PRICE.MONTHLY} per person /month
+          USD ${PLANS.EARLY_ADOPTER.PRICE.MONTHLY}
         </p>
         <p class="mt-4 text-sm font-light leading-6 text-white lg:leading-6">
-          Advanced features for small teaching businesses. Built for scale.
+          {PLANS.EARLY_ADOPTER.DESCRIPTION}
         </p>
-        <button
-          class="text-md mt-4 w-full rounded-md bg-white py-3 font-medium text-slate-900 hover:bg-indigo-50 lg:rounded-md lg:py-3 lg:text-lg lg:font-semibold"
-          data-cal-config={"{'layout':'month_view'}"}
-          data-cal-link="classroomio/demo"
+        <a
+          class="block hover:no-underline text-center text-md mt-4 w-full rounded-md bg-white py-3 font-medium text-slate-900 hover:bg-indigo-50 lg:rounded-md lg:py-3 lg:text-lg lg:font-semibold"
+          target="_blank"
+          href={PLANS.EARLY_ADOPTER.CTA.LINK}
         >
-          Get Started
-        </button>
+          {PLANS.EARLY_ADOPTER.CTA.LABEL}
+        </a>
         <ul class="mt-4 space-y-2 text-sm font-light text-white">
-          {#each PLANS.PRO.FEATURES as proFeature}
+          {#each PLANS.EARLY_ADOPTER.FEATURES as proFeature}
             <li class="flex items-center">
               <div>
                 <Checkmark size={16} fill="#1D4EE2" class="mr-2 lg:mr-3 " />
@@ -68,29 +70,29 @@
 
       <!-- card 3 -->
       <div class="max-w-xl p-4 ring-1 ring-gray-200 lg:max-w-sm">
-        <p class="text-lg font-semibold leading-8 text-gray-900 lg:text-xl">Team</p>
-        <p class="flex items-baseline gap-x-1 font-medium text-baseline">
-          USD ${PLANS.TEAM.PRICE.MONTHLY} per person /month
+        <p class="text-lg font-semibold leading-8 text-gray-900 lg:text-xl">
+          {PLANS.ENTERPRISE.NAME}
         </p>
+        <p class="flex items-baseline gap-x-1 font-medium text-baseline">Request Pricing</p>
         <p class=" mt-4 text-sm font-light leading-6 text-gray-500 lg:leading-6">
-          Best suited for teams and larger businesses that need more control.
+          {PLANS.ENTERPRISE.DESCRIPTION}
         </p>
         <button
           class="text-md mt-4 w-full rounded-md bg-slate-900 py-3 font-medium text-white hover:bg-slate-700 lg:rounded-md lg:py-3 lg:text-lg lg:font-semibold"
           data-cal-config={"{'layout':'month_view'}"}
-          data-cal-link="classroomio/demo"
+          data-cal-link="classroomio/enterprise"
         >
-          Get started
+          {PLANS.ENTERPRISE.CTA.LABEL}
         </button>
 
         <ul class="mt-4 space-y-2 text-sm text-gray-600">
-          {#each PLANS.TEAM.FEATURES as teamFeature}
+          {#each PLANS.ENTERPRISE.FEATURES as Feature}
             <li class="flex items-center">
               <div>
                 <Checkmark size={16} fill="#1D4EE2" class="mr-2 lg:mr-3 " />
               </div>
               <p class="text-sm">
-                {teamFeature}
+                {Feature}
               </p>
             </li>
           {/each}
