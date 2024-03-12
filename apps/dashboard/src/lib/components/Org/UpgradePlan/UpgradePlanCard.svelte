b2218afa9ca5f0d09577693f56460c66f004070c
Rotimi Best <best@classroomio.com>
2024-03-12 13:51:13 +0100

    use relative import

--- apps/dashboard/src/lib/components/Org/UpgradePlan/UpgradePlanCard.svelte
+++ apps/dashboard/src/lib/components/Org/UpgradePlan/UpgradePlanCard.svelte
@@ -1,24 +1,24 @@
 <script>
   import Checkmark from 'carbon-icons-svelte/lib/Checkmark.svelte';
-  import PLANS from '../../../../../../../packages/shared-constants/src/plans.json';
+  import PLANS from 'shared-constants/src/plans.json';
 </script>
 
 <section id="pricing">
   <div class="flex flex-col items-center justify-center">
-    <div class="isolate grid gap-1 grid-cols-1 lg:grid-cols-3">
+    <div class="isolate grid grid-cols-1 gap-1 lg:grid-cols-3">
       <!-- card 1 -->
-      <div class="max-w-xl p-4 ring-1 ring-gray-200 lg:max-w-sm rounded-xl">
-        <p class="text-lg font-semibold leading-8 text-gray-900 lg:text-xl mb-2">
+      <div class="max-w-xl rounded-xl p-4 ring-1 ring-gray-200 lg:max-w-sm">
+        <p class="mb-2 text-lg font-semibold leading-8 text-gray-900 lg:text-xl">
           {PLANS.BASIC.NAME}
         </p>
-        <p class="flex items-baseline gap-x-1 font-medium text-baseline">
+        <p class="text-baseline flex items-baseline gap-x-1 font-medium">
           USD ${PLANS.BASIC.PRICE.MONTHLY}
         </p>
         <p class=" mt-4 text-sm font-light leading-6 text-gray-500 lg:leading-6">
           {PLANS.BASIC.DESCRIPTION}
         </p>
         <a
-          class="block hover:no-underline text-center text-md mt-4 w-full rounded-md bg-slate-900 py-3 font-medium text-white hover:bg-slate-700 lg:rounded-md lg:py-3 lg:text-lg lg:font-semibold"
+          class="text-md mt-4 block w-full rounded-md bg-slate-900 py-3 text-center font-medium text-white hover:bg-slate-700 hover:no-underline lg:rounded-md lg:py-3 lg:text-lg lg:font-semibold"
           target="_blank"
           href={PLANS.BASIC.CTA.LINK}
         >
@@ -39,18 +39,18 @@
       </div>
 
       <!-- card 2 -->
-      <div class="max-w-xl bg-gray-900 ring-1 rounded-xl ring-gray-200 p-4 lg:max-w-sm">
-        <p class="text-lg font-semibold leading-8 text-white lg:text-xl mb-2">
+      <div class="max-w-xl rounded-xl bg-gray-900 p-4 ring-1 ring-gray-200 lg:max-w-sm">
+        <p class="mb-2 text-lg font-semibold leading-8 text-white lg:text-xl">
           {PLANS.EARLY_ADOPTER.NAME}
         </p>
-        <p class="flex items-baseline gap-x-1 text-baseline text-white font-medium">
+        <p class="text-baseline flex items-baseline gap-x-1 font-medium text-white">
           USD ${PLANS.EARLY_ADOPTER.PRICE.MONTHLY}
         </p>
         <p class="mt-4 text-sm font-light leading-6 text-white lg:leading-6">
           {PLANS.EARLY_ADOPTER.DESCRIPTION}
         </p>
         <a
-          class="block hover:no-underline text-center text-md mt-4 w-full rounded-md bg-white py-3 font-medium text-slate-900 hover:bg-indigo-50 lg:rounded-md lg:py-3 lg:text-lg lg:font-semibold"
+          class="text-md mt-4 block w-full rounded-md bg-white py-3 text-center font-medium text-slate-900 hover:bg-indigo-50 hover:no-underline lg:rounded-md lg:py-3 lg:text-lg lg:font-semibold"
           target="_blank"
           href={PLANS.EARLY_ADOPTER.CTA.LINK}
         >
@@ -71,11 +71,11 @@
       </div>
 
       <!-- card 3 -->
-      <div class="max-w-xl p-4 ring-1 rounded-xl ring-gray-200 lg:max-w-sm">
-        <p class="text-lg font-semibold leading-8 text-gray-900 lg:text-xl mb-2">
+      <div class="max-w-xl rounded-xl p-4 ring-1 ring-gray-200 lg:max-w-sm">
+        <p class="mb-2 text-lg font-semibold leading-8 text-gray-900 lg:text-xl">
           {PLANS.ENTERPRISE.NAME}
         </p>
-        <p class="flex items-baseline gap-x-1 font-medium text-baseline">Request Pricing</p>
+        <p class="text-baseline flex items-baseline gap-x-1 font-medium">Request Pricing</p>
         <p class=" mt-4 text-sm font-light leading-6 text-gray-500 lg:leading-6">
           {PLANS.ENTERPRISE.DESCRIPTION}
         </p>
