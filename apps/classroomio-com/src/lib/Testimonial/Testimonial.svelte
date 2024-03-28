fc80d940a7e244e80c1c19c2250bf692c1bd267d
Rotimi Best <best@classroomio.com>
2024-03-28 04:21:02 +0100

    feat: add testimonial to landing page

---
+++ apps/classroomio-com/src/lib/Testimonial/Testimonial.svelte
@@ -0,0 +1,21 @@
+<section id="testimonial" class="flex flex-col items-center py-[5%] px-3 lg:px-0">
+  <h1
+    class="mx-auto text-3xl md:text-4xl lg:text-6xl font-normal leading-[1.5] flex flex-col items-center gap-4"
+  >
+    Wall of Love
+  </h1>
+
+  <p
+    class="mx-auto text-center mt-10 lg:mt-6 px-4 lg:p-0 text-md tracking-wide leading-6 text-gray-400 lg:w-2/4"
+  >
+    What are people saying about ClassroomIO?
+  </p>
+
+  <div class="relative my-5 md:my-10 w-[85%] lg:w-[70%]">
+    <div
+      class="senja-embed"
+      data-id="3e847d7a-13d3-483e-8a71-5b35273dcc6c"
+      data-lazyload="false"
+    ></div>
+  </div>
+</section>
