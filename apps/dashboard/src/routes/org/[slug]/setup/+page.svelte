edefe75b7d16d7ff4e8d5dc5e03e7172b578ecf5
emmanuel <chifez1@gmail.com>
2024-03-28 09:48:56 +0100

    finished the UI of the onBoarding setup

--- apps/dashboard/src/routes/org/[slug]/setup/+page.svelte
+++ apps/dashboard/src/routes/org/[slug]/setup/+page.svelte
@@ -5,16 +5,21 @@
 
   export let data;
   let setupList = [];
+  let completed = 0;
 
   $: setupList = data.setup;
-  console.log(data);
+  $: completed = setupList.filter((list) => list.isCompleted).length;
 </script>
 
 <section>
   <div class="py-2 md:py-10 px-2 md:px-5">
     <div class="flex items-baseline gap-2">
-      <h1 class="dark:text-white text-2xl md:text-3xl font-bold">Courses</h1>
-      <Chip value="0/6" className="text-[10px] font-semibold px-3 !py-1" shape="rounded-full" />
+      <h1 class="dark:text-white text-2xl md:text-3xl font-bold">Get Started</h1>
+      <Chip
+        value={`${completed}/${setupList.length}`}
+        className="text-[10px] font-semibold px-3 !py-1"
+        shape="rounded-full"
+      />
     </div>
 
     <section class="px-4">
@@ -22,16 +27,18 @@
         <div
           class="flex flex-col lg:flex-row lg:items-center justify-between gap-2 w-full py-8 border-b border-gray-300"
         >
-          <div class="space-y-2 flex-1">
+          <div class={`${list.isCompleted ? 'text-[#656565]' : ''} space-y-2 flex-1`}>
             <div class="flex items-center gap-2">
               <Chip
                 value={i + 1}
-                className="text-[10px] font-semibold !py-1"
+                className={`${
+                  list.isCompleted ? 'bg-gray-400' : ''
+                } text-[10px] font-semibold !py-1 `}
                 shape="rounded-full"
               />
               <p class="font-semibold text-xl">{list.title}</p>
             </div>
-            <p class="text-sm">{list.desc}</p>
+            <p class={`text-sm`}>{list.desc}</p>
           </div>
           <div class="w-fit ml-auto lg:w-[15vw]">
             {#if list.isCompleted}
