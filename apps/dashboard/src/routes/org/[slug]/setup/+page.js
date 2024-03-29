e1f7c77a254c48b901f45953b5024bd4dcc08a42
emmanuel <chifez1@gmail.com>
2024-03-29 10:25:59 +0100

    Implementing the functions to carryout the logic of updating the UI onmount if a user has carried out a certain step or not

--- apps/dashboard/src/routes/org/[slug]/setup/+page.js
+++ apps/dashboard/src/routes/org/[slug]/setup/+page.js
@@ -1,42 +1,11 @@
-export const load = ({ params = { slug: '' } }) => {
-  const data = [
-    {
-      title: 'Create Course',
-      desc: 'Create a course that you will share with your students',
-      isCompleted: true,
-      buttonLabel: 'Create Course'
-    },
-    {
-      title: 'Upload a profile picture and update username',
-      desc: 'Personalize and a human touch making interactions more personal and memorable',
-      isCompleted: false,
-      buttonLabel: 'Update Profile'
-    },
-    {
-      title: 'Update organisation profile picture',
-      desc: 'Establish a professional and recognizable identity for your organization',
-      isCompleted: true,
-      buttonLabel: 'Update Org Profile'
-    },
-    {
-      title: 'Create a lesson',
-      desc: 'Break your course into lesson that your students can easily understand',
-      isCompleted: false,
-      buttonLabel: 'Create Lesson'
-    },
-    {
-      title: 'Create an exercise',
-      desc: 'Test your students allow them to demonstarte their understanding of the subject matter',
-      isCompleted: false,
-      buttonLabel: 'Create Assignment'
-    },
-    {
-      title: 'Publish a course',
-      desc: 'Make your course public and purchaseable ',
-      isCompleted: false,
-      buttonLabel: 'Publish Course'
-    }
-  ];
+import { getSupabase } from '$lib/utils/functions/supabase';
+// import { getServerSupabase } from '$lib/utils/functions/supabase.server';
+const supabase = getSupabase();
+export const load = async ({ params = { slug: '' } }) => {
+  const { data } = await supabase
+    .from('onboarding_setup')
+    .select('*')
+    .order('id', { ascending: true });
 
   return {
     orgSiteName: params.slug,
--- apps/dashboard/src/routes/org/[slug]/setup/+page.svelte
+++ apps/dashboard/src/routes/org/[slug]/setup/+page.svelte
@@ -2,13 +2,189 @@
   import Chip from '$lib/components/Chip/Text.svelte';
   import PrimaryButton from '$lib/components/PrimaryButton/index.svelte';
   import StepDoneIcon from '$lib/components/Icons/StepDoneIcon.svelte';
+  import { supabase } from '$lib/utils/functions/supabase';
+  import { courses, courseMetaDeta } from '$lib/components/Courses/store';
+  import { currentOrg, currentOrgPath } from '$lib/utils/store/org';
+  import { onMount } from 'svelte';
+  import { goto } from '$app/navigation';
+  import { snackbar } from '$lib/components/Snackbar/store.js';
 
   export let data;
-  let setupList = [];
+  let setupList = data.setup || [];
+  let coursesList = $courses;
   let completed = 0;
 
-  $: setupList = data.setup;
-  $: completed = setupList.filter((list) => list.isCompleted).length;
+  const StepsEnum = {
+    CREATE_COURSE: 'Create Course',
+    UPDATE_PROFILE: 'Upload a profile picture and update username',
+    UPDATE_ORG_PROFILE: 'Update organisation profile picture',
+    CREATE_LESSON: 'Create a lesson',
+    CREATE_EXERCISE: 'Create an exercise',
+    PUBLISH_COURSE: 'Publish a course'
+  };
+
+  // setting the database is_completed to true or false on mount
+
+  // check if the user has any course
+  // if true set create course to true;
+  // loop through the array of course and check if they have any lesson
+  // if true set the create lesson to true
+  // also loop through the array of lessons and check if the lessons has any exercise,if true set the create exercise to true
+
+  //UTILITY FUNCTIONS
+  function hasCourse() {
+    return coursesList.length > 0;
+  }
+
+  function hasLesson(course) {
+    return course.total_lessons > 0;
+  }
+
+  function isPublished(course) {
+    return course.is_published;
+  }
+
+  function hasExercise(course) {
+    //TODO
+    //check if any lesson in a course has an exercise
+    //probably loop through the lessons in the course
+    return false;
+  }
+
+  function checkUserProfile() {
+    //TODO
+    //get the user profile details and check if there is an avatar_url different from the default avatar_url
+    // update supabase accordingly
+    return false;
+  }
+
+  function checkOrgProfile() {
+    //TODO
+    //get current org details and check if there is an avatar url different from the default avatar_url
+    // update supabase accordingly
+    return false;
+  }
+
+  async function updateCompletion() {
+    let hasAnyCourse = false;
+    let anyCourseHasLessons = false;
+    let anyCourseIsPublished = false;
+
+    for (const step of setupList) {
+      switch (step.title) {
+        case StepsEnum.CREATE_COURSE:
+          if (!hasAnyCourse) {
+            hasAnyCourse = hasCourse();
+          }
+          await updateIsCompleted(step.id, hasAnyCourse);
+          break;
+
+        case StepsEnum.CREATE_LESSON:
+          if (!anyCourseHasLessons) {
+            anyCourseHasLessons = coursesList.some((course) => hasLesson(course));
+          }
+          await updateIsCompleted(step.id, anyCourseHasLessons);
+          break;
+
+        case StepsEnum.CREATE_EXERCISE:
+          const createLessonStep = setupList.find((item) => item.title === StepsEnum.CREATE_LESSON);
+          if (createLessonStep && createLessonStep.is_completed) {
+            const anyCourseHasExercises = coursesList.some((course) => hasExercise(course));
+            await updateIsCompleted(step.id, anyCourseHasExercises);
+          }
+          break;
+
+        case StepsEnum.PUBLISH_COURSE:
+          if (!anyCourseIsPublished) {
+            anyCourseIsPublished = coursesList.some((course) => isPublished(course));
+          }
+          await updateIsCompleted(step.id, anyCourseIsPublished);
+          break;
+
+        case StepsEnum.UPDATE_PROFILE:
+          const userHasProfile = checkUserProfile();
+          await updateIsCompleted(step.id, userHasProfile);
+          break;
+
+        case StepsEnum.UPDATE_ORG_PROFILE:
+          const orgHasProfile = checkOrgProfile();
+          await updateIsCompleted(step.id, orgHasProfile);
+          break;
+      }
+    }
+  }
+
+  async function updateIsCompleted(itemId, value) {
+    const { data, error } = await supabase
+      .from('onboarding_setup')
+      .update({ is_completed: value })
+      .eq('id', itemId);
+    const updated = setupList?.find((list) => list.id === itemId);
+    if (updated && setupList) {
+      updated.is_completed = value;
+      setupList = [...setupList];
+    }
+    console.log('itemId', itemId, updated);
+    if (error) {
+      console.error('Error updating is_completed:', error.message);
+    }
+  }
+
+  const handleClick = (title) => {
+    switch (title) {
+      case StepsEnum.CREATE_COURSE:
+        goto(`/org/${$currentOrg.siteName}/courses?create=true`);
+
+        break;
+
+      case StepsEnum.CREATE_LESSON:
+        if (hasCourse()) {
+          const courseId = coursesList[0].id;
+          goto(`/courses/${courseId}`);
+        } else {
+          snackbar.error('No course has been created');
+        }
+        break;
+
+      case StepsEnum.CREATE_EXERCISE:
+        for (const course of coursesList) {
+          //TODO
+          // we need to navigate to the first lesson,but what if the lesson is locked?
+          // then we navigate to the next availale unlocked lesson and if there is none, throw an error
+          if (hasLesson(course)) {
+            const id = course.lessons?.[0].id;
+            goto(`/courses/${id}`);
+          } else {
+            snackbar.error('You Need to Create a lesson');
+          }
+        }
+        break;
+
+      case StepsEnum.PUBLISH_COURSE:
+        if (hasCourse()) {
+          const courseId = coursesList[0].id;
+          goto(`/courses/${courseId}`);
+        } else {
+          snackbar.error('You need to creat a course');
+        }
+        break;
+
+      case StepsEnum.UPDATE_PROFILE:
+        goto(`/org/${$currentOrg.siteName}/settings`);
+        break;
+
+      case StepsEnum.UPDATE_ORG_PROFILE:
+        goto(`/org/${$currentOrg.siteName}/settings?tab=org`);
+        break;
+    }
+  };
+
+  $: completed = setupList.filter((list) => list.is_completed).length;
+
+  onMount(async () => {
+    await updateCompletion();
+  });
+  $: console.log('data', data);
 </script>
 
 <section>
@@ -27,12 +203,12 @@
         <div
           class="flex flex-col lg:flex-row lg:items-center justify-between gap-2 w-full py-8 border-b border-gray-300"
         >
-          <div class={`${list.isCompleted ? 'text-[#656565]' : ''} space-y-2 flex-1`}>
+          <div class={`${list.is_completed ? 'text-[#656565]' : ''} space-y-2 flex-1`}>
             <div class="flex items-center gap-2">
               <Chip
                 value={i + 1}
                 className={`${
-                  list.isCompleted ? 'bg-gray-400' : ''
+                  list.is_completed ? 'bg-gray-400' : ''
                 } text-[10px] font-semibold !py-1 `}
                 shape="rounded-full"
               />
@@ -41,15 +217,15 @@
             <p class={`text-sm`}>{list.desc}</p>
           </div>
           <div class="w-fit ml-auto lg:w-[15vw]">
-            {#if list.isCompleted}
+            {#if list.is_completed}
               <div class="w-full flex justify-center">
                 <StepDoneIcon />
               </div>
             {:else}
               <PrimaryButton
-                label={list.buttonLabel}
+                label={list.button_label}
                 className="!w-full font-normal text-sm"
-                onClick={() => (list.isCompleted = true)}
+                onClick={() => handleClick(list.title)}
               />
             {/if}
           </div>
