c291e43eec2fb92705db9aa502892e0f164c4608
Rotimi Best <best@classroomio.com>
2024-04-19 23:30:23 +0100

    nkkh

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   // ...rest of the Cypress project config
   projectId: '56i2dj',
-  defaultCommandTimeout: 600000,
+  defaultCommandTimeout: 60000,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
