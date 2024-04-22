32e10302cb24a268aa83eb928aad2896e56fbec2
Adeyemi Adetunji <tvnji01@gmail.com>
2024-04-22 22:17:33 +0100

    feat: teachers page (#275)
    * feat: add a teacher route
    
    * refactor
    
    * PR Fixes
    
    * text-md fix
    
    * remove root
    
    * fixes
    
    * update content
    
    * Update Register behavior
    
    ---------
    
    Co-authored-by: Rotimi Best <best@classroomio.com>

--- apps/classroomio-com/src/routes/blog/+page.svelte
+++ apps/classroomio-com/src/routes/blog/+page.svelte
@@ -2,6 +2,7 @@
   import Chip from '$lib/Chip/Chip.svelte';
   import { formatDate } from '$lib/utils/formatDate';
   import PageSignupCTA from '$lib/PageSignupCTA/index.svelte';
+  import PageHeader from '$lib/PageHeader/PageHeader.svelte';
 
   export let data;
 </script>
@@ -10,10 +11,8 @@
   <title>Blog | ClassroomIO</title>
 </svelte:head>
 
-<div class="mt-[10%] md:mt-16">
-  <header
-    class="hero-bg lg:min-h-[30rem] pt-24 pb-16 w-full flex flex-col items-center justify-center text-center border-b border-slate-100"
-  >
+<section>
+  <PageHeader className="flex flex-col items-center justify-center text-center">
     <h1
       class="mx-auto text-4xl md:text-7xl lg:text-6xl font-bold text-slate-900 flex flex-col items-center"
     >
@@ -23,7 +22,7 @@
     <p class="w-[90%] md:w-[60%] text-center font-normal text-lg text-slate-700 mt-10 lg:mt-7">
       Get the latest news from ClassroomIO, including product updates, team announcements and more!
     </p>
-  </header>
+  </PageHeader>
   <div class="w-full flex items-center justify-center">
     <ul class="flex flex-col items-start justify-start gap-2 w-[90%] md:w-[50%]">
       {#each data.posts as post}
@@ -66,4 +65,4 @@
     link="/signup"
     demo={false}
   />
-</div>
+</section>
