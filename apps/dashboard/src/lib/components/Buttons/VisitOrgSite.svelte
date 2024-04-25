bd94594b56aab8b8057c594250945e8b5e193ac1
Rotimi Best <best@classroomio.com>
2024-04-25 06:51:39 +0100

    feat: students can view org landing page from LMS

--- apps/dashboard/src/lib/components/Buttons/VisitOrgSite.svelte
+++ apps/dashboard/src/lib/components/Buttons/VisitOrgSite.svelte
@@ -5,14 +5,22 @@
   import { currentOrgDomain } from '$lib/utils/store/org';
   import { isMobile } from '$lib/utils/store/useMobile';
   import { t } from '$lib/utils/functions/translations';
+  import { user } from '$lib/utils/store/user';
 
   export let className = '';
+  export let isLMS = false;
+
+  $: href = isLMS && $user.isLoggedIn ? `${$currentOrgDomain}/home` : $currentOrgDomain;
 </script>
 
-<a href={$currentOrgDomain} target="_blank" class="{className} ml-2 hover:no-underline">
+<a {href} target="_blank" class="{className} ml-2 hover:no-underline">
   <PrimaryButton variant={VARIANTS.CONTAINED_DARK}>
     {#if !$isMobile}
-      {$t('settings.subheadings.view_site')}
+      {#if isLMS}
+        {$t('dashboard.visit_site')}
+      {:else}
+        {$t('settings.subheadings.view_site')}
+      {/if}
     {/if}
     <ArrowUpRightIcon />
   </PrimaryButton>
