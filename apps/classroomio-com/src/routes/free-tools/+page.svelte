f7022f1d7aa62bf9bc3ce7e0fcff0b2a477f65f2
tunny17 <tvnji01@gmail.com>
2024-05-02 13:51:40 +0100

    checkpoint

--- apps/classroomio-com/src/routes/free-tools/+page.svelte
+++ apps/classroomio-com/src/routes/free-tools/+page.svelte
@@ -4,8 +4,21 @@
 
 <section>
   <PageHeader>
-    <div class="text-center border-4 border-black">
-      <h1>Explore our free <span class="text-[#1D4ED8]">tools</span></h1>
+    <div class="text-center w-2/4 mx-auto">
+      <h1 class="text-6xl font-bold">Explore our free <span class="text-[#1D4ED8]">tools</span></h1>
+      <p class="my-5">
+        Innovative tools to help you aid you example preparation, as well as making learning more
+        fun for students and educator looking for engaging resources.
+      </p>
+
+      <div class="flex">
+        <img class="w-[10%]" src="/free-tools/waec.svg" alt="" />
+        <img class="w-[10%]" src="/free-tools/question-of-the-day.svg" alt="" />
+        <img class="w-[10%]" src="/free-tools/activity-timer.svg" alt="" />
+        <img class="w-[10%]" src="/free-tools/name-picker.svg" alt="" />
+        <img class="w-[10%]" src="/free-tools/tic-tac.svg" alt="" />
+        <img class="w-[10%]" src="/free-tools/jamb.svg" alt="" />
+      </div>
     </div>
   </PageHeader>
 </section>
