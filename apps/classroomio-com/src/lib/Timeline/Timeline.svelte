ae66221293f04c5dcc28d753501ac61461ead907
emmanuel <chifez1@gmail.com>
2024-05-23 15:33:10 +0100

    finished the timeline

---
+++ apps/classroomio-com/src/lib/Timeline/Timeline.svelte
@@ -0,0 +1,27 @@
+<script>
+  export let items = [];
+</script>
+
+<div class="relative w-fit flex justify-center space-x-5 md:space-x-10">
+  <div class="w-[3px] md:w-[2px] bg-blue-600 mt-10" />
+  <div class="space-y-8">
+    {#each items as card, index}
+      <div class="md:w-[40rem] w-full border rounded-md px-4 py-8 space-y-4">
+        <div class="flex items-center">
+          <div
+            class={`z-50 absolute -left-3 ${
+              index > 0 &&
+              'before:bg-gradient-to-t before:from-white before:to-transparent before:w-[4px] before:h-[9rem] before:absolute before:-top-36'
+            } flex items-center justify-center w-7 h-7 bg-blue-100 rounded-full`}
+          >
+            <div class="w-4 h-4 bg-blue-800 rounded-full z-50" />
+          </div>
+          <h1 class="text-2xl font-bold text-blue-800">{card.title}</h1>
+        </div>
+        <p class="text-sm text-gray-500">
+          {card.desc}
+        </p>
+      </div>
+    {/each}
+  </div>
+</div>
---
+++ apps/classroomio-com/src/routes/roadmap/+page.svelte
@@ -0,0 +1,76 @@
+<script>
+  import PageHeader from '$lib/PageHeader/PageHeader.svelte';
+  import Timeline from '$lib/Timeline/Timeline.svelte';
+
+  let newFeatures = [
+    {
+      title: 'Course Newsfeed',
+      desc: 'Meet our LMS newsfeed centralizes updates, announcements, and interactions, enhancing communication, engagement, and real-time access to course information.'
+    },
+    {
+      title: 'Security RLS',
+      desc: 'Meet our LMS newsfeed centralizes updates, announcements, and interactions, enhancing communication, engagement, and real-time access to course information.'
+    },
+    {
+      title: 'Increased video limit',
+      desc: 'Meet our LMS newsfeed centralizes updates, announcements, and interactions, enhancing communication, engagement, and real-time access to course information.'
+    },
+    {
+      title: 'LMS Customization',
+      desc: 'Meet our LMS newsfeed centralizes updates, announcements, and interactions, enhancing communication, engagement, and real-time access to course information.'
+    },
+    {
+      title: 'More certificate option',
+      desc: 'Meet our LMS newsfeed centralizes updates, announcements, and interactions, enhancing communication, engagement, and real-time access to course information.'
+    },
+    {
+      title: 'Multi language support',
+      desc: 'Meet our LMS newsfeed centralizes updates, announcements, and interactions, enhancing communication, engagement, and real-time access to course information.'
+    },
+    {
+      title: 'Course Newsfeed',
+      desc: 'Meet our LMS newsfeed centralizes updates, announcements, and interactions, enhancing communication, engagement, and real-time access to course information.'
+    }
+  ];
+
+  let upcomingFeatures = [
+    {
+      title: 'Learning path',
+      desc: 'Meet our LMS newsfeed centralizes updates, announcements, and interactions, enhancing communication, engagement, and real-time access to course information.'
+    },
+    {
+      title: 'Landing page builder',
+      desc: 'Meet our LMS newsfeed centralizes updates, announcements, and interactions, enhancing communication, engagement, and real-time access to course information.'
+    }
+  ];
+</script>
+
+<svelte:head>
+  <title>Roadmap | ClassroomIO</title>
+</svelte:head>
+<PageHeader className="flex flex-col items-center justify-center text-center space-y-10">
+  <h1 class="text-7xl font-bold">Roadmap</h1>
+  <p class="text-center w-[40%]">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto blanditiis, illo beatae
+    earum voluptatum ea suscipit consequatur veritatis labore accusantium.
+  </p>
+</PageHeader>
+<section class="flex flex-col items-center w-full px-4 mb-10">
+  <div class="flex flex-col items-start space-y-4">
+    <div>
+      <h1 class="text-3xl font-bold">Latest Features</h1>
+    </div>
+
+    <Timeline items={newFeatures} />
+  </div>
+</section>
+
+<section class="flex flex-col items-center w-full px-4">
+  <div class="flex flex-col items-start space-y-4">
+    <div>
+      <h1 class="text-3xl font-bold">Upcoming Features</h1>
+    </div>
+
+    <Timeline items={upcomingFeatures} />
+  </div>
+</section>
