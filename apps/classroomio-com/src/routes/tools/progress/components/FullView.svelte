d36ed9ce192353d0a28f5130751898a07978e618
tunny17 <tvnji01@gmail.com>
2024-05-29 16:14:40 +0100

    fix: full screen

--- apps/classroomio-com/src/routes/tools/progress/components/FullView.svelte
+++ apps/classroomio-com/src/routes/tools/progress/components/FullView.svelte
@@ -5,9 +5,9 @@
 
 {#if $openFullscreen.open}
   <div class="relative">
-    <div class="fixed z-[3000] top-0 left-0 w-full h-full bg-black opacity-[0.9]"></div>
+    <div class="fixed z-[3000] top-0 left-0 w-full h-full bg-black opacity-[0.4]"></div>
     <div
-      class="fixed top-0 left-0 md:top-[20%] md:left-[35%] z-[3001] w-full h-full md:h-fit md:w-[30%] min-w-[500px] bg-white rounded-md mx-auto flex items-center md:block py-6 px-2 md:px-7"
+      class="fixed top-5 left-0 md:top-[5%] md:left-[30%] z-[3001] w-full h-fit md:w-[30%] md:min-w-[500px] bg-white rounded-md mx-auto flex items-center md:block py-6 px-2 md:px-7"
     >
       <!--  -->
       <Report />
