af01166b91cef5bde38707008b53f6cf159d6e2d
Rotimi Best <best@classroomio.com>
2024-06-17 00:29:16 +0100

    use adapter auto

--- apps/classroomio-com/svelte.config.js
+++ apps/classroomio-com/svelte.config.js
@@ -1,5 +1,5 @@
 import sequence from 'svelte-sequential-preprocessor';
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import { fileURLToPath } from 'node:url';
 import { dirname } from 'node:path';
@@ -30,16 +30,7 @@ const mdsvexOptions = {
 
 const config = {
   kit: {
-    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
-    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter({
-      // See below for an explanation of these options
-      routes: {
-        include: ['/*'],
-        exclude: ['<all>']
-      }
-    })
+    adapter: adapter()
   },
   extensions: ['.svelte', '.md'],
   preprocess: sequence([vitePreprocess(), mdsvex(mdsvexOptions)])
