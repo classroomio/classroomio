01c15eda9c60358b6d8886c9e1db769a8330b075
Iniubong Obonguko <iniubongobonguko2018@gmail.com>
2024-06-18 12:55:43 +0100

    chore: add id to render component

--- apps/dashboard/src/lib/components/HTMLRender/HTMLRender.svelte
+++ apps/dashboard/src/lib/components/HTMLRender/HTMLRender.svelte
@@ -1,9 +1,10 @@
 <script>
   export let content = '';
   export let className = '';
+  export let id = '';
 </script>
 
-<article class="preview prose prose-sm sm:prose {className} dark:text-white">
+<article {id} class="preview prose prose-sm sm:prose {className} dark:text-white">
   {#if $$slots.content}
     <slot name="content" />
   {:else}
