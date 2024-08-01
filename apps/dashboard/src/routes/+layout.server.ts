99f236b70d4e8bdd4a01d1f628857f2280c897a2
Rotimi Best <best@classroomio.com>
2024-08-01 14:04:50 +0100

    feat: support course meta tags (#421)
    * support course meta tags
    
    * fix import

--- apps/dashboard/src/routes/+layout.server.ts
+++ apps/dashboard/src/routes/+layout.server.ts
@@ -1,3 +1,4 @@
+import type { MetaTagsProps } from 'svelte-meta-tags';
 import { dev, browser } from '$app/environment';
 import { redirect } from '@sveltejs/kit';
 import { blockedSubdomain } from '$lib/utils/constants/app';
@@ -16,6 +17,7 @@ interface LoadOutput {
   isOrgSite: boolean;
   skipAuth: boolean;
   org: CurrentOrg | null;
+  baseMetaTags: MetaTagsProps;
 }
 
 export const load = async ({ url, cookies }): Promise<LoadOutput> => {
@@ -23,7 +25,8 @@ export const load = async ({ url, cookies }): Promise<LoadOutput> => {
     orgSiteName: '',
     isOrgSite: false,
     skipAuth: false,
-    org: null
+    org: null,
+    baseMetaTags: getBaseMetaTags(url)
   };
 
   // Selfhosted usecase would be here
@@ -97,6 +100,44 @@ export const load = async ({ url, cookies }): Promise<LoadOutput> => {
 
   return response;
 };
+
+function getBaseMetaTags(url: URL) {
+  return Object.freeze({
+    title: 'ClassroomIO – Launch Your Online Bootcamp In Minutes',
+    description:
+      'Launch your bootcamp quickly and affordably with ClassroomIO, the customizable online teaching platform.',
+    canonical: new URL(url.pathname, url.origin).href,
+    openGraph: {
+      type: 'website',
+      url: new URL(url.pathname, url.origin).href,
+      locale: 'en_IE',
+      title: 'ClassroomIO – Launch Your Online Bootcamp In Minutes',
+      description:
+        'Launch your bootcamp quickly and affordably with ClassroomIO, the customizable online teaching platform.',
+      siteName: 'ClassroomIO',
+      images: [
+        {
+          url: 'https://classroomio.com/classroomio-opengraph-image.png',
+          alt: 'ClassroomIO OG Image',
+          width: 1920,
+          height: 1080,
+          secureUrl: 'https://classroomio.com/classroomio-opengraph-image.png',
+          type: 'image/jpeg'
+        }
+      ]
+    },
+    twitter: {
+      handle: '@classroomio',
+      site: '@classroomio',
+      cardType: 'summary_large_image',
+      title: 'ClassroomIO – Launch Your Online Bootcamp In Minutes',
+      description:
+        'Launch your bootcamp quickly and affordably with ClassroomIO, the customizable online teaching platform.',
+      image: 'https://classroomio.com/classroomio-opengraph-image.png',
+      imageAlt: 'ClassroomIO OG Image'
+    }
+  });
+}
 
 // Define getInitialLocale function
 function getInitialLocale(): string {
--- apps/dashboard/src/routes/course/[slug]/+page.svelte
+++ apps/dashboard/src/routes/course/[slug]/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import get from 'lodash/get';
   import CourseLandingPage from '$lib/components/CourseLandingPage/index.svelte';
   import type { Course } from '$lib/utils/types';
 
@@ -19,33 +18,5 @@
 
   $: setCourseData(data.course);
 </script>
-
-<svelte:head>
-  <title>{get(data.course, 'title', '')}</title>
-  <meta name="description" content={get(data.course, 'description', '')} />
-  <meta name="theme-color" content="#ffffff" />
-  <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <meta property="og:title" content={get(data.course, 'title', '')} />
-  <meta property="og:description" content={get(data.course, 'description', '')} />
-
-  <meta property="og:type" content="website" />
-  <meta property="og:image:type" content="image/png" />
-  <meta property="og:image:width" content="280" />
-  <meta property="og:image:height" content="200" />
-  <meta property="og:image:secure_url" content={get(data.course, 'logo', '')} />
-
-  <meta property="og:type" content="website" />
-  <meta property="og:image:type" content="image/png" />
-  <meta property="og:image:width" content="1920" />
-  <meta property="og:image:height" content="1080" />
-  <meta property="og:image:secure_url" itemprop="image" content={get(data.course, 'logo', '')} />
-
-  <meta name="twitter:card" content="summary_large_image" />
-  <meta property="twitter:domain" content="classroomio.com" />
-  <meta property="twitter:url" content="https://www.classroomio.com/" />
-  <meta name="twitter:title" content={get(data.course, 'title', '')} />
-  <meta name="twitter:description" content={get(data.course, 'description', '')} />
-  <meta name="twitter:image" content={get(data.course, 'logo', '')} />
-</svelte:head>
 
 <CourseLandingPage {courseData} />
