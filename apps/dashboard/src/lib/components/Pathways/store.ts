cccfed2b7f0b799892d1d7115cb00d16c2d2a331
Adeyemi Adetunji <tvnji01@gmail.com>
2024-08-17 21:06:22 +0100

    feat: Pathway People Page (#425)
    * feat: People Page
    
    * refactor
    
    ---------
    
    Co-authored-by: Rotimi Best <best@classroomio.com>

--- apps/dashboard/src/lib/components/Pathways/store.ts
+++ apps/dashboard/src/lib/components/Pathways/store.ts
@@ -1,5 +1,5 @@
 import { writable } from 'svelte/store';
-import type { GroupStore, Pathway, PathwayCourse } from '$lib/utils/types';
+import type { Pathway, PathwayCourse, GroupStore } from '$lib/utils/types';
 
 export const addCourseModal = writable({
   open: false,
@@ -141,87 +141,27 @@ export const group = writable<GroupStore>({
     {
       assigned_student_id: 1,
       created_at: '',
-      email: '',
-      group_id: '',
-      id: '',
-      memberId: '',
-      profile: {},
+      email: 'testemail@gmail.com',
+      group_id: '3bdeddc4-aea9-4f7f-a099-9d094f031a39',
+      id: '3a4fbb86-0d44-4f8d-a903-54b4a7396f48',
+      memberId: '3a4fbb86-0d44-4f8d-a903-54b4a7396f48',
+      profile: {
+        avatar_url:
+          'https://tapaozmyjsuykgerrfkt.supabase.co/storage/v1/object/public/avatars/avatar.png',
+        can_add_course: true,
+        created_at: '2024-05-28T12:23:00.585402+00:00',
+        email: 'testemail@gmail.com',
+        fullname: 'Frank Ocean',
+        goal: '',
+        id: '5e3c306f-d1b0-4ccb-8d48-3ae140dc1f60',
+        role: '1',
+        source: 'search-engine',
+        updated_at: '2024-05-28T12:23:00.585402+00:00',
+        username: 'admin1716898979376'
+      },
       profile_id: '3d207582-eaf8-4cff-9314-f265f548c0e7',
       role_id: 3,
-      fullname: '',
+      fullname: 'Frank Ocean'
     }
   ]
 });
-
-export async function setPathway(data: Pathway, setPathway = true) {
-  // if (!data || !(Object.values(data) && Object.values(data).length)) return;
-  // // const tutorsById = {};
-
-  // if (data.group) {
-  //   const groupData = Object.assign(data.group, {
-  //     tutors: [],
-  //     students: [],
-  //     people: []
-  //   }) as GroupStore;
-
-  //   if (Array.isArray(groupData.members)) {
-  //     for (const member of groupData.members) {
-  //       if (member.role_id === ROLE.STUDENT) {
-  //         groupData.students.push(member);
-  //       } else if (member.profile) {
-  //         groupData.tutors.push({
-  //           ...member.profile,
-  //           memberId: member.id
-  //         });
-  //       }
-  //     }
-
-  //     groupData.people = groupData.members;
-  //   }
-
-  //   delete groupData.members;
-
-  //   group.set(groupData);
-  // }
-
-  // if (setLesson) {
-  //   const orderedLessons = (data.lessons || [])
-  //     .sort((a, b) => new Date(a.created_at).getTime() - new Date(b.created_at).getTime())
-  //     .sort((a, b) => (a.order ?? 0) - (b.order ?? 0));
-  //   // .map((lesson) => ({
-  //   //   ...lesson,
-  //   //   profile: lesson.profile && tutorsById[lesson.profile.id],
-  //   // }));
-  //   lessons.set(orderedLessons);
-  // }
-
-  // delete data.lessons;
-
-  // if (data.metadata && !Object.values(data.metadata)) {
-  //   data.metadata = {
-  //     requirements: '',
-  //     description: '',
-  //     goals: '',
-  //     videoUrl: '',
-  //     showDiscount: false,
-  //     discount: 0,
-  //     reward: {
-  //       show: false,
-  //       description: ''
-  //     },
-  //     instructor: {
-  //       name: '',
-  //       role: '',
-  //       coursesNo: 0,
-  //       description: '',
-  //       imgUrl: ''
-  //     },
-  //     allowNewStudent: false
-  //   };
-  // }
-
-  // if (!data.certificate_theme) {
-  //   data.certificate_theme = 'professional';
-  // }
-  // course.set(data);
-}
