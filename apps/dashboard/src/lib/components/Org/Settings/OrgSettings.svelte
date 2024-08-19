ea93be95756c1dd6b4b37c29db7a5e189b42939a
Rotimi Best <best@classroomio.com>
2024-08-19 19:26:41 +0100

    fix: theme switching

--- apps/dashboard/src/lib/components/Org/Settings/OrgSettings.svelte
+++ apps/dashboard/src/lib/components/Org/Settings/OrgSettings.svelte
@@ -13,6 +13,7 @@
   import { t } from '$lib/utils/functions/translations';
   import { isFreePlan } from '$lib/utils/store/org';
   import { updateOrgNameValidation } from '$lib/utils/functions/validator';
+  import { setTheme } from '$lib/utils/functions/theme';
 
   let avatar;
 
@@ -42,6 +43,8 @@
         .concat(!!t ? ' ' : '', t);
       $currentOrg.theme = t;
 
+      setTheme(t);
+
       const res = await supabase
         .from('organization')
         .update({ theme: t })
@@ -63,7 +66,7 @@
       loading = true;
 
       const updates = {
-        name: $currentOrg.name,
+        name: $currentOrg.name
       };
 
       if (avatar) {
