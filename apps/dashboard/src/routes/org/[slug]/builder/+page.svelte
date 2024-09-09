c6f0f35e2ad3ff04577267c7e01a7ef263ca8655
tunny17 <tvnji01@gmail.com>
2024-09-09 07:46:47 +0100

    feat: add select func

--- apps/dashboard/src/routes/org/[slug]/builder/+page.svelte
+++ apps/dashboard/src/routes/org/[slug]/builder/+page.svelte
@@ -6,20 +6,31 @@
   const templates = [
     {
       id: 1,
+      title: 'Exam Prep',
+      image: '/exam-prep'
+    },
+    {
+      id: 2,
       title: 'Bootcamps',
       image: '/bootcamp'
     },
     {
-      id: 2,
+      id: 3,
       title: 'Expert/Individual',
       image: '/individual'
     },
     {
-      id: 3,
+      id: 4,
       title: 'Academy/Organization',
       image: '/organization'
     }
   ];
+
+  let currentTemplate = templates[0];
+
+  const selectTemplate = (template) => {
+    currentTemplate = template;
+  };
 </script>
 
 <svelte:head>
@@ -33,16 +44,21 @@
   </header>
 
   <section class="mt-8">
+    <!-- current image -->
     <div class="border rounded-md">
       <div
         class="bg-[#f1f6ff] px-5 md:px-10 pt-5 md:pt-8 rounded-tl-md rounded-tr-md max-h-[50vh] overflow-hidden overflow-y-auto"
       >
-        <img src="/images/builder-templates/current-template.png" alt="Current Org Template" />
+        <img
+          class="w-full"
+          src="/images/builder-templates/{currentTemplate.image}-template.png"
+          alt="{currentTemplate.title} Template"
+        />
       </div>
 
       <div class="px-10 py-4 flex gap-y-5 flex-col md:flex-row justify-between md:items-center">
         <div>
-          <p class="text-base font-semibold">{$t('template_builder.prep')}</p>
+          <p class="text-base font-semibold">{currentTemplate.title}</p>
           <p class="text-xs mt-1">{$t('template_builder.added')} 5 {$t('template_builder.days')}</p>
         </div>
         <PrimaryButton className="w-full md:w-fit" variant={VARIANTS.CONTAINED_DARK}
@@ -54,11 +70,12 @@
     <div class="my-10">
       <h1 class="font-bold text-lg">{$t('template_builder.more')}</h1>
 
-      <div class="flex flex-wrap gap-y-5 justify-between items-center">
+      <div class="flex flex-wrap gap-y-5 justify-between">
+        <!-- templates -->
         {#each templates as template}
-          <div class="border rounded-md md:max-w-[31%]">
+          <div class="border rounded-md w-full md:max-w-[31%]">
             <div
-              class="relative bg-[#f1f6ff] px-5 pt-4 rounded-tl-md rounded-tr-md max-h-[40vh] md:max-h-[27vh] overflow-hidden"
+              class="relative bg-[#f1f6ff] px-5 pt-4 rounded-tl-md rounded-tr-md h-full max-h-[40vh] md:max-h-[27vh] overflow-hidden"
             >
               <img
                 class="md:hover:scale-105 duration-300 transition-all"
@@ -72,10 +89,15 @@
               >
                 <button
                   type="button"
-                  on:click={() => console.log("template's id", template.id)}
+                  on:click={() => selectTemplate(template)}
                   class="text-black font-semibold text-sm py-2 px-4 bg-white rounded"
-                  >{$t('template_builder.preview')}</button
                 >
+                  {#if currentTemplate.id === template.id}
+                    Added
+                  {:else}
+                    {$t('template_builder.preview')}
+                  {/if}
+                </button>
               </div>
             </div>
 
@@ -83,8 +105,15 @@
               <p class="text-sm font-semibold">{template.title}</p>
               <button
                 type="button"
+                disabled={currentTemplate.id === template.id}
                 class="text-xs font-medium px-2.5 py-1 border border-[#282828] rounded-sm"
-                >{$t('template_builder.add')}</button
+                on:click={() => selectTemplate(template)}
+              >
+                {#if currentTemplate.id === template.id}
+                  Added
+                {:else}
+                  {$t('template_builder.add')}
+                {/if}</button
               >
             </div>
           </div>
