080d31f0b96a56990f114e2accf8e686ee641e47
Rotimi Best <best@classroomio.com>
2024-11-18 21:10:44 +0100

    chore: clean up navigation

--- apps/classroomio-com/src/lib/Navigation/Navigation.svelte
+++ apps/classroomio-com/src/lib/Navigation/Navigation.svelte
@@ -3,45 +3,44 @@
   import TextAlignJustify from 'carbon-icons-svelte/lib/TextAlignJustify.svelte';
   import CloseLarge from 'carbon-icons-svelte/lib/CloseLarge.svelte';
   import { fly } from 'svelte/transition';
-  import MachineLearningModel from 'carbon-icons-svelte/lib/MachineLearningModel.svelte';
   import ForumIcon from 'carbon-icons-svelte/lib/Forum.svelte';
   import CourseIcon from '$lib/Icons/CourseIcon.svelte';
   import MapCenter from 'carbon-icons-svelte/lib/MapCenter.svelte';
   import { page } from '$app/stores';
 
-  let showsubNav = false;
-  let showNav = false;
+  let showDrawer = false;
+  let showSolutions = false;
   let activeLink = '';
 
-  function handleShow() {
-    showsubNav = !showsubNav;
+  function handleShowDrawer() {
+    showDrawer = !showDrawer;
   }
 
-  function handleShowNav() {
-    showNav = !showNav;
+  function handleShowSolutions() {
+    showSolutions = !showSolutions;
   }
 
   const solutions = [
     {
       key: 'employee-training',
       title: 'Employee Training',
-      subtitle: 'Personalized learning experience'
+      subtitle: 'Keep your team in sync.'
     },
     {
       key: 'bootcamps',
       title: 'Bootcamps',
-      subtitle: 'Drive student achievement.'
+      subtitle: 'Drive student satisfaction.'
     },
     {
       key: 'customer-education',
       title: 'Customer Education',
-      subtitle: 'Get customers up to speed quickly'
+      subtitle: 'Teach customers your product.'
     }
   ];
 
   $: activeLink = $page.url.pathname;
   $: activeHash = $page.url.hash;
-  $: isSolutionsActive = solutions.some((sl) => activeHash.includes(sl.key));
+  $: isSolutionsActive = solutions.some((s) => activeHash.includes(s.key));
 </script>
 
 <div
@@ -65,23 +64,25 @@
     <ul class="flex items-center justify-between w-full gap-2">
       <li class="text-gray-800 font-semibold text-sm cursor-pointer relative">
         <button
-          class="flex items-center hover:bg-gray-100 px-4 py-2 rounded-md"
-          on:click={() => (showNav = !showNav)}
+          on:focus={() => (showSolutions = true)}
+          on:mouseenter={() => (showSolutions = true)}
+          class="flex items-center hover:bg-gray-100 {showSolutions &&
+            'bg-gray-100'} px-4 py-2 rounded-md"
+          on:click={() => (showSolutions = !showSolutions)}
           class:active={isSolutionsActive}
         >
-          Solutions <ChevronDown class="ml-2" />
+          Solutions <ChevronDown size={16} class="ml-2" />
         </button>
-        {#if showNav}
-          <div
-            class="absolute w-[24rem] top-10 -left-10 border px-5 py-5 rounded-[30px] shadow-slate-700 z-[3001] bg-white"
+        {#if showSolutions}
+          <button
+            class="absolute w-[20rem] top-10 -left-10 border rounded-lg px-2 py-4 shadow-slate-700 z-[3001] bg-white flex flex-col gap-3"
+            on:mouseleave={() => (showSolutions = false)}
           >
             {#each solutions as solution}
               <a
-                class="flex justify-between items-center w-full rounded-lg hover:bg-gray-100 p-5 mb-4"
+                class="flex justify-between items-center w-full rounded-lg hover:bg-gray-100 p-2"
                 href="/{solution.key}"
-                on:click={() => {
-                  showNav = !showNav;
-                }}
+                on:click={() => (showSolutions = false)}
               >
                 {#if solution.key === 'employee-training'}
                   <CourseIcon />
@@ -90,7 +91,8 @@
                 {:else if solution.key === 'customer-education'}
                   <ForumIcon size={24} />
                 {/if}
-                <div class="w-[86%] text-start">
+
+                <div class="w-[84%] text-start">
                   <h3 class="font-semibold text-sm text-gray-700">
                     {solution.title}
                   </h3>
@@ -100,7 +102,7 @@
                 </div>
               </a>
             {/each}
-          </div>
+          </button>
         {/if}
       </li>
       <a
@@ -110,14 +112,6 @@
       >
         <li class="hover:bg-gray-100 px-4 py-2 rounded-md">Free Tools</li>
       </a>
-      <!-- <a
-        href="/docs"
-        target="_blank"
-        class="text-gray-800 font-semibold text-sm cursor-pointer"
-        class:active={activeLink.startsWith('/docs')}
-      >
-        <li class="hover:bg-gray-100 px-4 py-2 rounded-md">Docs</li>
-      </a> -->
 
       <a
         href="/blog"
@@ -165,10 +159,12 @@
     type="button"
     aria-label="Hamburger Menu"
     class="block md:block lg:hidden"
-    on:click={handleShowNav}><TextAlignJustify size={24} /></button
+    on:click={handleShowSolutions}
   >
+    <TextAlignJustify size={24} />
+  </button>
 
-  {#if showNav}
+  {#if showSolutions}
     <div
       in:fly={{ x: 20, duration: 700 }}
       out:fly={{ x: 20, duration: 400 }}
@@ -183,25 +179,25 @@
           alt="classroomio logo"
           class="w-[15%]"
         />
-        <button on:click={handleShowNav}><CloseLarge size={24} class="mr-5" /></button>
+        <button on:click={handleShowSolutions}><CloseLarge size={24} class="mr-5" /></button>
       </div>
       <nav>
         <ul class="flex items-center flex-col lg:flex-row justify-between w-full">
           <li class="text-gray-800 font-semibold text-sm md:text-lg cursor-pointer w-full">
             <button
               class="w-full flex items-center justify-between hover:bg-gray-100 py-3 px-4 rounded-lg"
-              on:click={handleShow}
+              on:click={handleShowDrawer}
               class:active={isSolutionsActive}
             >
               Our Superpowers <ChevronDown />
             </button>
-            {#if showsubNav}
+            {#if showDrawer}
               <div in:fly={{ y: -20, duration: 700 }} out:fly={{ y: 20, duration: 400 }}>
                 {#each solutions as solution}
                   <a
-                    href="/#{solution.key}"
+                    href="/{solution.key}"
                     on:click={() => {
-                      handleShowNav();
+                      handleShowSolutions();
                     }}
                   >
                     <p
@@ -217,7 +213,7 @@
           <!-- Free Tools -->
           <a
             class="text-gray-800 font-semibold text-sm md:text-lg cursor-pointer hover:bg-gray-100 py-3 px-4 rounded-xl w-full"
-            on:click={handleShowNav}
+            on:click={handleShowSolutions}
             href="/tools"
           >
             <li>Free Tools</li>
@@ -225,7 +221,7 @@
           <!-- <a
             class="text-gray-800 font-semibold text-sm md:text-lg cursor-pointer hover:bg-gray-100 py-3 px-4 rounded-md w-full"
             on:click={() => {
-              handleShowNav();
+              handleShowSolutions();
             }}
             class:active={activeLink.startsWith('/docs')}
             href="/docs"
@@ -236,7 +232,7 @@
           <a
             class="text-gray-800 font-semibold text-sm md:text-lg cursor-pointer hover:bg-gray-100 py-3 px-4 rounded-md w-full"
             on:click={() => {
-              handleShowNav();
+              handleShowSolutions();
             }}
             class:active={activeLink.startsWith('/blog')}
             href="/blog"
@@ -247,7 +243,7 @@
           <a
             class="text-gray-800 font-semibold text-sm md:text-lg cursor-pointer hover:bg-gray-100 py-3 px-4 rounded-xl w-full"
             on:click={() => {
-              handleShowNav();
+              handleShowSolutions();
             }}
             href="/pricing"
             class:active={activeLink.startsWith('/pricing')}
@@ -257,7 +253,7 @@
           <a
             class="text-gray-800 font-semibold text-sm md:text-lg cursor-pointer hover:bg-gray-100 py-3 px-4 rounded-md w-full"
             on:click={() => {
-              handleShowNav();
+              handleShowSolutions();
             }}
             class:active={activeHash.includes('morefeatures')}
             href="/#morefeatures"
