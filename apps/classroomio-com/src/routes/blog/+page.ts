7e239da7d241cdea7f036ab6fe66676883bfc2f7
Dunsin <78784850+Dun-sin@users.noreply.github.com>
2024-12-01 20:03:42 +0100

    feat: add recommended blog post depending on current post tags (#514)

--- apps/classroomio-com/src/routes/blog/+page.ts
+++ apps/classroomio-com/src/routes/blog/+page.ts
@@ -1,5 +1,6 @@
 import type { BlogPost } from '$lib/types';
 
+/** @type {import('./$types').PageLoad} */
 export async function load({ fetch }) {
   const response = await fetch('api/blog');
   const posts: BlogPost[] = await response.json();
