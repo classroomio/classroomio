ec0d107b93ba8a590828478e8dc6d941c414aae8
emmanuel <chifez1@gmail.com>
2024-12-04 16:57:12 +0100

    analytics UI

--- apps/dashboard/src/routes/org/[slug]/analytics/+page.svelte
+++ apps/dashboard/src/routes/org/[slug]/analytics/+page.svelte
@@ -1,54 +1,31 @@
 <script>
-  import { onMount } from 'svelte';
-  import { goto } from '$app/navigation';
-  import { page } from '$app/stores';
+  import { PUBLIC_ANALYTICS_URL } from '$env/static/public';
+  import { globalStore } from '$lib/utils/store/app';
+  import { currentOrg } from '$lib/utils/store/org';
 
-  let site = 'classroomio.com';
+  let site = '';
+  const BASE_URL =
+    import.meta.env.MODE === 'development'
+      ? 'http://localhost:3005/dashboard'
+      : PUBLIC_ANALYTICS_URL;
 
-  function handleMessage(event) {
-    // For development, we only accept messages from localhost:3005
-    if (event.origin !== 'http://localhost:3005') {
-      console.warn('Received message from unexpected origin:', event.origin);
-      return;
-    }
-
-    try {
-      const { type, params } = event.data;
-      if (type === 'urlUpdate' && params) {
-        // Update the URL in the parent window without page refresh
-        const newUrl = `${$page.url.pathname}?${new URLSearchParams(params)}`;
-        goto(newUrl, {
-          replaceState: true, // This prevents page refresh
-          keepFocus: true, // Maintains focus on the current element
-          noScroll: true // Prevents scrolling to top
-        });
-      }
-    } catch (error) {
-      console.error('Error processing message:', error);
-    }
-  }
-
-  // Listen for messages from the iframe
-  onMount(() => {
-    window.addEventListener('message', handleMessage);
-    return () => {
-      window.removeEventListener('message', handleMessage);
-    };
-  });
-
-  // Get initial URL parameters
-  $: initialParams = $page.url.searchParams;
-  $: iframeUrl = `http://localhost:3005/dashboard?site=${site}${
-    initialParams ? '&' + initialParams : ''
-  }`;
-  $: console.log('refresh token');
+  $: site = $currentOrg.name || '';
 </script>
 
-<iframe
-  src={iframeUrl}
-  width="100%"
-  height="100%"
-  frameborder="0"
-  scrolling="yes"
-  title="Analytics Dashboard"
-></iframe>
+{#if site != ''}
+  {#key $globalStore.isDark}
+    <iframe
+      src={`${BASE_URL}?site=${site}&theme=${$globalStore.isDark}`}
+      width="100%"
+      height="100%"
+      frameborder="0"
+      scrolling="yes"
+      title="Analytics Dashboard"
+      class="dark:text-white"
+    ></iframe>
+  {/key}
+{:else}
+  <div class="flex items-center">
+    <p>LOADING ANALYTICS...</p>
+  </div>
+{/if}
