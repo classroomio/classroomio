bcb8a4b295965d6561240cfbcf8792213412b9e8
Rotimi Best <best@classroomio.com>
2025-12-21 19:26:07 +0000

    chore: update .npmrc to set npm registry and remove vercel.json configuration

--- apps/docs/vite.config.ts
+++ apps/docs/vite.config.ts
@@ -21,15 +21,16 @@ export default defineConfig({
     react(),
     // see https://tanstack.com/start/latest/docs/framework/react/guide/hosting for hosting config
     // we configured nitro by default
-    nitro({
-      routeRules: {
-        '/': {
-          proxy: 'https://classroomio-com.vercel.app/'
-        },
-        '/:match*': {
-          proxy: 'https://classroomio-com.vercel.app/:match*'
-        }
-      }
-    })
+    nitro()
+    // {
+    //   routeRules: {
+    //     '/': {
+    //       proxy: 'https://classroomio-com.vercel.app/'
+    //     },
+    //     '/:match*': {
+    //       proxy: 'https://classroomio-com.vercel.app/:match*'
+    //     }
+    //   }
+    // }
   ]
 });
