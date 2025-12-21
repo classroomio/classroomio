abe91c76599817c622294714a5349ad05dde002e
Rotimi Best <best@classroomio.com>
2025-12-21 09:29:03 +0000

    add /docs

--- apps/docs-v2/src/router.tsx
+++ apps/docs-v2/src/router.tsx
@@ -1,12 +1,13 @@
+import { NotFound } from '@/components/not-found';
 import { createRouter as createTanStackRouter } from '@tanstack/react-router';
 import { routeTree } from './routeTree.gen';
-import { NotFound } from '@/components/not-found';
 
 export function getRouter() {
   return createTanStackRouter({
-    routeTree,
-    defaultPreload: 'intent',
-    scrollRestoration: true,
+    basepath: '/docs',
     defaultNotFoundComponent: NotFound,
+    defaultPreload: 'intent',
+    routeTree,
+    scrollRestoration: true
   });
 }
--- apps/docs-v2/vite.config.ts
+++ apps/docs-v2/vite.config.ts
@@ -1,29 +1,30 @@
-import react from '@vitejs/plugin-react';
-import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import { defineConfig } from 'vite';
-import tsConfigPaths from 'vite-tsconfig-paths';
-import tailwindcss from '@tailwindcss/vite';
 import mdx from 'fumadocs-mdx/vite';
 import { nitro } from 'nitro/vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import { tanstackStart } from '@tanstack/react-start/plugin/vite';
+import tsConfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   server: {
-    port: 3000,
+    port: 3000
   },
+  base: '/docs',
   plugins: [
     mdx(await import('./source.config')),
     tailwindcss(),
     tsConfigPaths({
-      projects: ['./tsconfig.json'],
+      projects: ['./tsconfig.json']
     }),
     tanstackStart({
       prerender: {
-        enabled: true,
-      },
+        enabled: true
+      }
     }),
     react(),
     // see https://tanstack.com/start/latest/docs/framework/react/guide/hosting for hosting config
     // we configured nitro by default
-    nitro(),
-  ],
+    nitro()
+  ]
 });
