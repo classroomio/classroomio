4bcff3813a284d5bd37e18dff3638b4538078466
Rotimi Best <best@classroomio.com>
2026-05-19 05:28:50 +0100

    chore: worker setup for docs

--- apps/docs/react-router.config.ts
+++ apps/docs/react-router.config.ts
@@ -1,5 +1,9 @@
 import type { Config } from '@react-router/dev/config';
 
 export default {
-  basename: '/docs'
+  ssr: true,
+  basename: '/docs',
+  future: {
+    v8_viteEnvironmentApi: true
+  }
 } satisfies Config;
--- apps/docs/vite.config.ts
+++ apps/docs/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import mdx from 'fumadocs-mdx/vite';
 import { reactRouter } from '@react-router/dev/vite';
+import { cloudflare } from '@cloudflare/vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 import tsConfigPaths from 'vite-tsconfig-paths';
 
@@ -11,6 +12,7 @@ export default defineConfig({
   },
   base: '/docs',
   plugins: [
+    cloudflare({ viteEnvironment: { name: 'ssr' } }),
     mdx(await import('./source.config')),
     tailwindcss(),
     tsConfigPaths({
