7a30c4988059b1215ff08e512154e8e09feb301e
Rotimi Best <best@classroomio.com>
2026-06-22 05:46:45 +0100

    fix: add / to base path in vite

--- apps/docs/vite.config.ts
+++ apps/docs/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     host: '127.0.0.1',
     port: 3000
   },
-  base: '/docs',
+  base: '/docs/',
   ssr: {
     optimizeDeps: {
       include: ['fumadocs-ui/components/callout', 'fumadocs-ui/components/steps']
