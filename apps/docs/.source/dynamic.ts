5a6c4fb9c136a0790d5dccb35fb9f9a2d59b7fd3
Victor Jack <67293106+thedon-dev@users.noreply.github.com>
2026-07-24 15:41:09 +0100

    deleted unwanted files

--- apps/docs/.source/dynamic.ts
+++
@@ -1,9 +0,0 @@
-// @ts-nocheck
-/// <reference types="vite/client" />
-import { dynamic } from 'fumadocs-mdx/runtime/dynamic';
-import * as Config from '../source.config';
-
-const create = await dynamic<typeof Config, import("fumadocs-mdx/runtime/types").InternalTypeConfig & {
-  DocData: {
-  }
-}>(Config, {"environment":"vite","configPath":"source.config.ts","outDir":".source"}, {"doc":{"passthroughs":["extractedReferences"]}});
